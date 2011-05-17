376e8cf184fbfa8566529fcb4ec5dda3950a6828
Ben Wolfe <ben@openmrs.org>
2011-05-17 13:42:55 +0000

    Added some license headers

--- api/src/main/java/org/openmrs/module/webservices/rest/RuntimeWrappedException.java
+++ api/src/main/java/org/openmrs/module/webservices/rest/RuntimeWrappedException.java
@@ -1,16 +1,29 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.webservices.rest;
 
-
 /**
- * Used as a workaround for a few places where Spring will not let you throw a checked exception
- * (for example if you want to throw ObjectNotFoundException in a property editor.
+ * Used as a workaround for a few places where Spring will not let you throw a
+ * checked exception (for example if you want to throw ObjectNotFoundException
+ * in a property editor.
  */
 public class RuntimeWrappedException extends RuntimeException {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	public RuntimeWrappedException(Exception ex) {
 		super(ex);
 	}
-	
+
 }
