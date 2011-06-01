2b7839079615bcf378c93968cd1b0aacf2735b41
Daniel Kayiwa <dkayiwa@openmrs.org>
2011-06-01 12:14:23 +0000

    Refactoring package search utility based on review comments - RESTWS-3

--- omod/src/main/java/org/openmrs/module/webservices/rest/web/RestUtil.java
+++ omod/src/main/java/org/openmrs/module/webservices/rest/web/RestUtil.java
@@ -13,16 +13,23 @@
  */
 package org.openmrs.module.webservices.rest.web;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
-import javax.management.RuntimeErrorException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -562,5 +569,85 @@ public class RestUtil implements GlobalPropertyListener {
 	@Override
 	public void globalPropertyDeleted(String propertyName) {
 		setUriPrefix();
+	}
+	
+	/**
+	 * Gets a list of classes in a given package.
+	 * 
+	 * @param pkgname the package name.
+	 * @return the list of classes.
+	 */
+	public static ArrayList<Class<?>> getClassesForPackage(String pkgname) {
+		ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
+		
+		//Get a File object for the package
+		File directory = null;
+		String relPath = pkgname.replace('.', '/');
+		URL resource = ClassLoader.getSystemClassLoader().getResource(relPath);
+		if (resource == null) {
+			throw new RuntimeException("No resource for " + relPath);
+		}
+		
+		try {
+			directory = new File(resource.toURI());
+		}
+		catch (URISyntaxException e) {
+			throw new RuntimeException(pkgname + " (" + resource
+			        + ") does not appear to be a valid URL / URI.  Strange, since we got it from the system...", e);
+		}
+		
+		//If folder exists, look for all resource class files in it.
+		if (directory.exists()) {
+			
+			//Get the list of the files contained in the package
+			String[] files = directory.list();
+			
+			for (int i = 0; i < files.length; i++) {
+				
+				//We are only interested in Resource.class files
+				if (files[i].endsWith("Resource.class")) {
+					
+					//Remove the .class extension
+					String className = pkgname + '.' + files[i].substring(0, files[i].length() - 6);
+					
+					try {
+						classes.add(Class.forName(className));
+					}
+					catch (ClassNotFoundException e) {
+						throw new RuntimeException("ClassNotFoundException loading " + className);
+					}
+				}
+			}
+		} else {
+			
+			//Directory does not exist, look in jar file.
+			try {
+				String fullPath = resource.getFile();
+				String jarPath = fullPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
+				JarFile jarFile = new JarFile(jarPath);
+				
+				Enumeration<JarEntry> entries = jarFile.entries();
+				while (entries.hasMoreElements()) {
+					JarEntry entry = entries.nextElement();
+					
+					String entryName = entry.getName();
+					if (entryName.startsWith(relPath) && entryName.length() > (relPath.length() + "/".length())) {
+						String className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
+						
+						try {
+							classes.add(Class.forName(className));
+						}
+						catch (ClassNotFoundException e) {
+							throw new RuntimeException("ClassNotFoundException loading " + className);
+						}
+					}
+				}
+			}
+			catch (IOException e) {
+				throw new RuntimeException(pkgname + " (" + directory + ") does not appear to be a valid package", e);
+			}
+		}
+		
+		return classes;
 	}
 }
