15946885c739805ea08f6efe9fbb04da872e50b8
Darius Jazayeri <jazayeri@alum.mit.edu>
2012-04-16 22:26:03 +0000

    Implement support for forms - RESTWS-202 author: jkeiper

---
+++ omod/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/FieldAnswerResource.java
@@ -0,0 +1,141 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.webservices.rest.web.v1_0.resource;
+
+import java.util.List;
+
+import org.openmrs.FieldAnswer;
+import org.openmrs.annotation.Handler;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.ConversionUtil;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+
+/**
+ * {@link Resource} for {@link FieldAnswer}, supporting standard CRUD operations
+ * TODO THIS SHOULD PROBABLY BE A SUBRESOURCE UNDER FIELD, RIGHT?
+ */
+@Resource("fieldanswer")
+@Handler(supports = FieldAnswer.class, order = 0)
+public class FieldAnswerResource extends DelegatingCrudResource<FieldAnswer> {
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#getRepresentationDescription(org.openmrs.module.webservices.rest.web.representation.Representation)
+	 */
+	@Override
+	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
+		if (rep instanceof DefaultRepresentation) {
+			DelegatingResourceDescription description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("display", findMethod("getDisplayString"));
+			description.addProperty("concept", Representation.REF);
+			description.addProperty("field", Representation.REF);
+			description.addSelfLink();
+			description.addLink("full", ".?v=" + RestConstants.REPRESENTATION_FULL);
+			return description;
+		} else if (rep instanceof FullRepresentation) {
+			DelegatingResourceDescription description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("display", findMethod("getDisplayString"));
+			description.addProperty("concept");
+			description.addProperty("field");
+			description.addProperty("auditInfo", findMethod("getAuditInfo"));
+			description.addSelfLink();
+			return description;
+		}
+		return null;
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getByUniqueId(java.lang.String)
+	 */
+	@Override
+	public FieldAnswer getByUniqueId(String uniqueId) {
+		return Context.getFormService().getFieldAnswerByUuid(uniqueId);
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#newDelegate()
+	 */
+	@Override
+	protected FieldAnswer newDelegate() {
+		return new FieldAnswer();
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#save(java.lang.Object)
+	 */
+	@Override
+	protected FieldAnswer save(FieldAnswer delegate) {
+		throw new UnsupportedOperationException("A field answer must be added to a field, not created on its own.");
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#purge(java.lang.Object, org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	@Override
+	public void purge(FieldAnswer delegate, RequestContext context) throws ResponseException {
+		throw new UnsupportedOperationException("A field answer must be removed from a field, not purged on its own.");
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#doGetAll(org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	@Override
+	protected List<FieldAnswer> doGetAll(RequestContext context) throws ResponseException {
+		throw new UnsupportedOperationException(
+		        "Field answers must be retrieved from a field or by uuid, but not on their own.");
+	}
+	
+	@Override
+	protected void delete(FieldAnswer delegate, String reason, RequestContext context) throws ResponseException {
+		throw new UnsupportedOperationException("A field answer must be removed from a field, not deleted on its own.");
+	}
+	
+	/**
+	 * provide audit info for a FieldAnswer
+	 * 
+	 * @param delegate
+	 * @return the auditInfo
+	 * @throws Exception 
+	 */
+	public SimpleObject getAuditInfo(FieldAnswer delegate) throws Exception {
+		SimpleObject ret = new SimpleObject();
+		ret.put("creator", ConversionUtil.getPropertyWithRepresentation(delegate, "creator", Representation.REF));
+		ret.put("dateCreated", ConversionUtil.convertToRepresentation(delegate.getDateCreated(), Representation.DEFAULT));
+		return ret;
+	}
+	
+	/**
+	 * @param encounter
+	 * @return encounter type and date
+	 */
+	public String getDisplayString(FieldAnswer delegate) {
+		if (delegate == null)
+			return null;
+		
+		return new StringBuilder().append(delegate.getField() == null ? "Null Field" : delegate.getField().getName())
+		        .append(" - ").append(
+		            delegate.getConcept() == null ? "Null Concept" : delegate.getConcept().getName().toString()).toString();
+	}
+	
+}
---
+++ omod/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/FormFieldResource.java
@@ -0,0 +1,128 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.webservices.rest.web.v1_0.resource;
+
+import java.util.List;
+
+import org.openmrs.FormField;
+import org.openmrs.annotation.Handler;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
+import org.openmrs.module.webservices.rest.web.resource.impl.MetadataDelegatingCrudResource;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+
+/**
+ * {@link Resource} for {@link FormField}, supporting standard CRUD operations
+ */
+@Resource("formfield")
+@Handler(supports = FormField.class, order = 0)
+public class FormFieldResource extends MetadataDelegatingCrudResource<FormField> {
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#getRepresentationDescription(org.openmrs.module.webservices.rest.web.representation.Representation)
+	 */
+	@Override
+	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
+		if (rep instanceof DefaultRepresentation) {
+			DelegatingResourceDescription description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("display", findMethod("getDisplayString"));
+			description.addProperty("name");
+			description.addProperty("description");
+			description.addProperty("parent", Representation.REF);
+			description.addProperty("form", Representation.REF);
+			description.addProperty("field", Representation.REF);
+			description.addProperty("fieldNumber");
+			description.addProperty("fieldPart");
+			description.addProperty("pageNumber");
+			description.addProperty("minOccurs");
+			description.addProperty("maxOccurs");
+			description.addProperty("required");
+			description.addProperty("sortWeight");
+			description.addProperty("retired");
+			description.addSelfLink();
+			description.addLink("full", ".?v=" + RestConstants.REPRESENTATION_FULL);
+			return description;
+		} else if (rep instanceof FullRepresentation) {
+			DelegatingResourceDescription description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("display", findMethod("getDisplayString"));
+			description.addProperty("name");
+			description.addProperty("description");
+			description.addProperty("parent", Representation.REF);
+			description.addProperty("form");
+			description.addProperty("field");
+			description.addProperty("fieldNumber");
+			description.addProperty("fieldPart");
+			description.addProperty("pageNumber");
+			description.addProperty("minOccurs");
+			description.addProperty("maxOccurs");
+			description.addProperty("required");
+			description.addProperty("sortWeight");
+			description.addProperty("retired");
+			description.addProperty("auditInfo", findMethod("getAuditInfo"));
+			description.addSelfLink();
+			return description;
+		}
+		return null;
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getByUniqueId(java.lang.String)
+	 */
+	@Override
+	public FormField getByUniqueId(String uniqueId) {
+		return Context.getFormService().getFormFieldByUuid(uniqueId);
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#newDelegate()
+	 */
+	@Override
+	protected FormField newDelegate() {
+		return new FormField();
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#save(java.lang.Object)
+	 */
+	@Override
+	protected FormField save(FormField delegate) {
+		return Context.getFormService().saveFormField(delegate);
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#purge(java.lang.Object, org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	@Override
+	public void purge(FormField delegate, RequestContext context) throws ResponseException {
+		if (delegate == null)
+			return;
+		Context.getFormService().purgeFormField(delegate);
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#doGetAll(org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	@Override
+	protected List<FormField> doGetAll(RequestContext context) throws ResponseException {
+		return Context.getFormService().getAllFormFields();
+	}
+}
