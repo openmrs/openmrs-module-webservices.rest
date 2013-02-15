019c71436ccb79d1759da61c90c126447448c9b5
andreapat <apattson@earthlink.net>
2013-02-15 11:56:53 -0800

    RESTWS-322

--- omod/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/ConceptClassControllerTest.java
+++ omod/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/ConceptClassControllerTest.java
@@ -14,30 +14,20 @@
 package org.openmrs.module.webservices.rest.web.v1_0.controller.openmrs1_8;
 
 import java.util.List;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.beanutils.PropertyUtils;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.openmrs.ConceptClass;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.test.Util;
 import org.openmrs.module.webservices.rest.web.RestConstants;
-import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
-
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.request.WebRequest;
-
-import org.openmrs.module.webservices.rest.web.response.ResourceDoesNotSupportOperationException;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseCrudControllerTest;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.ResourceTestConstants;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Tests functionality of {@link ConceptClassController}.
@@ -46,16 +36,12 @@ public class ConceptClassControllerTest extends BaseCrudControllerTest {
 	
 	private ConceptService service;
 	
-	//private ConceptClassController controller;
-	private MockHttpServletRequest request;
-	
-	//	private HttpServletResponse response;
-	
 	private static final String ACTIVE_LIST_INITIAL_XML = "customActiveListTest.xml";
 	
 	@Before
 	public void init() throws Exception {
 		executeDataSet(ACTIVE_LIST_INITIAL_XML);
+		service = Context.getConceptService();
 	}
 	
 	@Test
@@ -70,13 +56,12 @@ public class ConceptClassControllerTest extends BaseCrudControllerTest {
 	
 	@Test
 	public void getConceptClass_shouldGetAConceptClassByUuid() throws Exception {
-		String conceptClassUuid = "ecdee8a7-d741-4fe7-8e01-f79cacbe97bc";
 		
-		MockHttpServletRequest req = request(RequestMethod.GET, getURI() + "/" + conceptClassUuid);
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI() + "/" + getUuid());
 		SimpleObject result = deserialize(handle(req));
 		Assert.assertNotNull(result);
 		Util.log("ConceptClass fetched (default)", result);
-		Assert.assertEquals(conceptClassUuid, result.get("uuid"));
+		Assert.assertEquals(getUuid(), result.get("uuid"));
 		
 	}
 	
@@ -99,20 +84,9 @@ public class ConceptClassControllerTest extends BaseCrudControllerTest {
 		Util.log("Found " + results.size() + " conceptClasse(s)", results);
 	}
 	
-	//	@Test
-	//	public void shouldRetireAConceptClass() throws Exception {
-	//		String uuid = "97097dd9-b092-4b68-a2dc-e5e5be961d42";
-	//		ConceptClass conceptClass = service.getConceptClassByUuid(uuid);
-	//		Assert.assertFalse(conceptClass.isRetired());
-	//		controller.delete(uuid, "test reason", request, response);
-	//		conceptClass = service.getConceptClassByUuid(uuid);
-	//		Assert.assertTrue(conceptClass.isRetired());
-	//		Assert.assertEquals("test reason", conceptClass.getRetireReason());
-	//	}
-	//
 	@Test
 	public void shouldRetireAConceptClass() throws Exception {
-		String uuid = "97097dd9-b092-4b68-a2dc-e5e5be961d42";
+		String uuid = "4719138c-8553-4c81-bb92-753f41f8be16";
 		
 		Assert.assertEquals(false, service.getConceptClassByUuid(uuid).isRetired());
 		MockHttpServletRequest req = request(RequestMethod.DELETE, getURI() + "/" + uuid);
@@ -124,17 +98,6 @@ public class ConceptClassControllerTest extends BaseCrudControllerTest {
 		Assert.assertEquals(reason, service.getConceptClassByUuid(uuid).getRetireReason());
 	}
 	
-	/*
-	public void shouldRetireAConceptClass() throws Exception {
-		String uuid = "97097dd9-b092-4b68-a2dc-e5e5be961d42";
-		
-		Assert.assertNotNull(service.getConceptClassByUuid(uuid));
-		MockHttpServletRequest req = request(RequestMethod.DELETE, getURI() + "/" + uuid);
-		req.addParameter("purge", "");
-		handle(req);
-		Assert.assertNull(service.getConceptClassByUuid(uuid));
-	}
-	*/
 	public void shouldPurgeAConceptClass() throws Exception {
 		String uuid = "77177ce7-1410-40ee-bbad-ff6905ee3095";
 		
@@ -145,21 +108,24 @@ public class ConceptClassControllerTest extends BaseCrudControllerTest {
 		Assert.assertNull(service.getConceptClassByUuid(uuid));
 	}
 	
-	//	@Test
-	//	public void shouldNotIncludeTheAuditInfoForTheDefaultRepresentation() throws Exception {
-	//		Object result = controller.retrieve("97097dd9-b092-4b68-a2dc-e5e5be961d42", request);
-	//		Assert.assertNotNull(result);
-	//		Assert.assertNull(PropertyUtils.getProperty(result, "auditInfo"));
-	//	}
-	//	
-	//@Test
-	//public void shouldIncludeTheAuditInfoForTheFullRepresentation() throws Exception {
-	//	MockHttpServletRequest httpReq = new MockHttpServletRequest();
-	//	httpReq.addParameter(RestConstants.REQUEST_PROPERTY_FOR_REPRESENTATION, RestConstants.REPRESENTATION_FULL);
-	//	Object result = controller.retrieve("97097dd9-b092-4b68-a2dc-e5e5be961d42", httpReq);
-	//	Assert.assertNotNull(result);
-	//	Assert.assertNotNull(PropertyUtils.getProperty(result, "auditInfo"));
-	//}
+	@Test
+	public void shouldNotIncludeTheAuditInfoForTheDefaultRepresentation() throws Exception {
+		String uuid = "97097dd9-b092-4b68-a2dc-e5e5be961d42";
+		
+		Object result = deserialize(handle(newGetRequest(getURI() + "/" + uuid)));
+		Assert.assertNotNull(result);
+		Assert.assertNull(PropertyUtils.getProperty(result, "auditInfo"));
+	}
+	
+	@Test
+	public void shouldIncludeTheAuditInfoForTheFullRepresentation() throws Exception {
+		String uuid = "97097dd9-b092-4b68-a2dc-e5e5be961d42";
+		MockHttpServletRequest httpReq = newGetRequest(getURI() + "/" + uuid, new Parameter(
+		        RestConstants.REQUEST_PROPERTY_FOR_REPRESENTATION, RestConstants.REPRESENTATION_FULL));
+		Object result = deserialize(handle(httpReq));
+		Assert.assertNotNull(result);
+		Assert.assertNotNull(PropertyUtils.getProperty(result, "auditInfo"));
+	}
 	
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.BaseCrudControllerTest#getURI()
