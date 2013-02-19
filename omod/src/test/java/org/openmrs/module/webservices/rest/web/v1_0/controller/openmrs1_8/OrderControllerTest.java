6e5fdfb491d3e46de3860ebd27a72c6723b139da
lluismf <lluismf@gmail.com>
2013-02-19 01:31:03 +0100

    RESTWS-334:Fix OrderControllerTest

--- omod/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/OrderControllerTest.java
+++ omod/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/OrderControllerTest.java
@@ -13,217 +13,279 @@
  */
 package org.openmrs.module.webservices.rest.web.v1_0.controller.openmrs1_8;
 
-import javax.servlet.http.HttpServletResponse;
-
-import junit.framework.Assert;
+import java.util.List;
 
 import org.apache.commons.beanutils.PropertyUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.DrugOrder;
+import org.openmrs.Order;
+import org.openmrs.Patient;
 import org.openmrs.api.OrderService;
+import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.test.Util;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
-import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseCrudControllerTest;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.ResourceTestConstants;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Integration tests for the Order resource
  */
-public class OrderControllerTest extends BaseModuleWebContextSensitiveTest {
+public class OrderControllerTest extends BaseCrudControllerTest {
+		
+	private final static String DRUG_ORDER_UUID = "921de0a3-05c4-444a-be03-e01b4c4b9142";
 	
-	//	private final static String ORDER_UUID = "ff97d3a0-8dbf-11e1-bc86-da3a922f3783";
-	//	
-	//	private final static String DRUG_ORDER_UUID = "921de0a3-05c4-444a-be03-e01b4c4b9142";
-	//	
-	//	private final static String PATIENT_UUID = "5946f880-b197-400b-9caa-a3c661d23041";
-	//	
-	//	private OrderService service;
-	//	
-	//	private OrderController controller;
-	//	
-	//	private MockHttpServletRequest request;
-	//	
-	//	private HttpServletResponse response;
-	//	
-	//	@Before
-	//	public void before() throws Exception {
-	//		executeDataSet("customTestDataset.xml");
-	//		this.service = Context.getOrderService();
-	//		this.controller = new OrderController();
-	//		this.request = new MockHttpServletRequest();
-	//		this.response = new MockHttpServletResponse();
-	//	}
-	//	
-	//	@Test
-	//	public void shouldGetOrderAsRef() throws Exception {
-	//		request.setParameter(RestConstants.REQUEST_PROPERTY_FOR_REPRESENTATION, Representation.REF.getRepresentation());
-	//		Object o = controller.retrieve(ORDER_UUID, request);
-	//		Assert.assertEquals(ORDER_UUID, PropertyUtils.getProperty(o, "uuid"));
-	//		Assert.assertNotNull(PropertyUtils.getProperty(o, "display"));
-	//		Assert.assertNull(PropertyUtils.getProperty(o, "concept"));
-	//		Util.log("order as ref", o);
-	//	}
-	//	
-	//	@Test
-	//	public void shouldGetOrderAsDefault() throws Exception {
-	//		Object o = controller.retrieve(ORDER_UUID, request);
-	//		Assert.assertEquals(ORDER_UUID, PropertyUtils.getProperty(o, "uuid"));
-	//		Assert.assertNotNull(PropertyUtils.getProperty(o, "display"));
-	//		Assert.assertNotNull(PropertyUtils.getProperty(o, "patient"));
-	//		Assert.assertNotNull(PropertyUtils.getProperty(o, "concept"));
-	//		Util.log("drug order as ref", o);
-	//	}
-	//	
-	//	@Test
-	//	public void shouldGetDrugOrderAsRef() throws Exception {
-	//		request.setParameter(RestConstants.REQUEST_PROPERTY_FOR_REPRESENTATION, Representation.REF.getRepresentation());
-	//		Object o = controller.retrieve(DRUG_ORDER_UUID, request);
-	//		Assert.assertEquals(DRUG_ORDER_UUID, PropertyUtils.getProperty(o, "uuid"));
-	//		Assert.assertNotNull(PropertyUtils.getProperty(o, "display"));
-	//		Assert.assertNull(PropertyUtils.getProperty(o, "concept"));
-	//	}
-	//	
-	//	@Test
-	//	public void shouldGetDrugOrderAsDefault() throws Exception {
-	//		Object o = controller.retrieve(DRUG_ORDER_UUID, request);
-	//		Assert.assertEquals(DRUG_ORDER_UUID, PropertyUtils.getProperty(o, "uuid"));
-	//		Assert.assertNotNull(PropertyUtils.getProperty(o, "display"));
-	//		Assert.assertNotNull(PropertyUtils.getProperty(o, "patient"));
-	//		Assert.assertNotNull(PropertyUtils.getProperty(o, "concept"));
-	//	}
-	//	
-	//	@Test
-	//	public void shouldGetAllOrders() throws Exception {
-	//		SimpleObject all = controller.getAll(request, response);
-	//		Assert.assertEquals(6, Util.getResultsSize(all));
-	//		// 5 should be drugorders, one should be a plain order
-	//		int numDrugOrders = 0;
-	//		int numOrders = 0;
-	//		for (Object o : Util.getResultsList(all)) {
-	//			if ("drugorder".equals(PropertyUtils.getProperty(o, "type")))
-	//				numDrugOrders += 1;
-	//			else if ("order".equals(PropertyUtils.getProperty(o, "type")))
-	//				numOrders += 1;
-	//		}
-	//		Assert.assertEquals(5, numDrugOrders);
-	//		Assert.assertEquals(1, numOrders);
-	//	}
-	//	
-	//	@Test
-	//	public void shouldGetAllDrugOrders() throws Exception {
-	//		request.setParameter(RestConstants.REQUEST_PROPERTY_FOR_TYPE, "drugorder");
-	//		SimpleObject all = controller.getAll(request, response);
-	//		Assert.assertEquals(5, Util.getResultsSize(all));
-	//		for (Object o : Util.getResultsList(all))
-	//			Assert.assertEquals("drugorder", PropertyUtils.getProperty(o, "type"));
-	//	}
-	//	
-	//	@Test
-	//	public void shouldGetAllOrdersByPatient() throws Exception {
-	//		SimpleObject all = controller.searchByPatient(PATIENT_UUID, request, response);
-	//		Assert.assertEquals(2, Util.getResultsSize(all));
-	//		// 1 should be drugorder, 1 should be a plain order
-	//		int numDrugOrders = 0;
-	//		int numOrders = 0;
-	//		for (Object o : Util.getResultsList(all)) {
-	//			if ("drugorder".equals(PropertyUtils.getProperty(o, "type")))
-	//				numDrugOrders += 1;
-	//			else if ("order".equals(PropertyUtils.getProperty(o, "type")))
-	//				numOrders += 1;
-	//		}
-	//		Assert.assertEquals(1, numDrugOrders);
-	//		Assert.assertEquals(1, numOrders);
-	//	}
-	//	
-	//	@Test
-	//	public void shouldGetAllDrugOrdersByPatient() throws Exception {
-	//		request.setParameter(RestConstants.REQUEST_PROPERTY_FOR_TYPE, "drugorder");
-	//		SimpleObject all = controller.searchByPatient(PATIENT_UUID, request, response);
-	//		Assert.assertEquals(1, Util.getResultsSize(all));
-	//		Assert.assertEquals("drugorder", Util.getByPath(all, "results[0]/type"));
-	//	}
-	//	
-	//	@Test
-	//	public void shouldCreateOrder() throws Exception {
-	//		String conceptUuid = "0dde1358-7fcf-4341-a330-f119241a46e8";
-	//		String orderTypeUuid = "e23733ab-787e-4096-8ba2-577a902d2c2b";
-	//		SimpleObject post = new SimpleObject().add("type", "order").add("patient", PATIENT_UUID).add("concept", conceptUuid)
-	//		        .add("orderType", orderTypeUuid);
-	//		Object o = controller.create(post, request, response);
-	//		Assert.assertEquals("order", PropertyUtils.getProperty(o, "type"));
-	//		Assert.assertEquals(conceptUuid, Util.getByPath(o, "concept/uuid"));
-	//		Assert.assertEquals(orderTypeUuid, Util.getByPath(o, "orderType/uuid"));
-	//		Assert.assertEquals(PATIENT_UUID, Util.getByPath(o, "patient/uuid"));
-	//	}
-	//	
-	//	@Test
-	//	public void shouldCreateDrugOrder() throws Exception {
-	//		String conceptUuid = "d144d24f-6913-4b63-9660-a9108c2bebef";
-	//		String drugUuid = "3cfcf118-931c-46f7-8ff6-7b876f0d4202";
-	//		SimpleObject post = new SimpleObject().add("type", "drugorder").add("patient", PATIENT_UUID).add("concept",
-	//		    conceptUuid).add("dose", "1").add("units", "tablet").add("drug", drugUuid);
-	//		Object o = controller.create(post, request, response);
-	//		Assert.assertEquals("drugorder", PropertyUtils.getProperty(o, "type"));
-	//		Assert.assertEquals(conceptUuid, Util.getByPath(o, "concept/uuid"));
-	//		Assert.assertEquals(PATIENT_UUID, Util.getByPath(o, "patient/uuid"));
-	//		Assert.assertEquals("tablet", PropertyUtils.getProperty(o, "units"));
-	//		Assert.assertEquals(drugUuid, Util.getByPath(o, "drug/uuid"));
-	//	}
-	//	
-	//	@Test
-	//	public void shouldUpdateOrder() throws Exception {
-	//		String newInstructions = "STAT!";
-	//		controller.update(ORDER_UUID, new SimpleObject().add("instructions", newInstructions), request, response);
-	//		Assert.assertEquals(newInstructions, service.getOrderByUuid(ORDER_UUID).getInstructions());
-	//	}
-	//	
-	//	@Test
-	//	public void shouldUpdateDrugOrder() throws Exception {
-	//		controller.update(DRUG_ORDER_UUID, new SimpleObject().add("dose", "500"), request, response);
-	//		Assert.assertEquals(500d, ((DrugOrder) service.getOrderByUuid(DRUG_ORDER_UUID)).getDose());
-	//	}
-	//	
-	//	@Test(expected = IllegalArgumentException.class)
-	//	public void shouldFailToChangeOrderClass() throws Exception {
-	//		controller.update(ORDER_UUID, new SimpleObject().add("type", "drugorder").add("instructions", "This will fail"),
-	//		    request, response);
-	//	}
-	//	
-	//	@Test
-	//	public void shouldDeleteOrder() throws Exception {
-	//		Assert.assertFalse(service.getOrderByUuid(ORDER_UUID).isVoided());
-	//		controller.delete(ORDER_UUID, "because", request, response);
-	//		Assert.assertTrue(service.getOrderByUuid(ORDER_UUID).isVoided());
-	//	}
-	//	
-	//	@Test
-	//	public void shouldDeleteDrugOrder() throws Exception {
-	//		Assert.assertFalse(service.getOrderByUuid(DRUG_ORDER_UUID).isVoided());
-	//		controller.delete(DRUG_ORDER_UUID, "because", request, response);
-	//		Assert.assertTrue(service.getOrderByUuid(DRUG_ORDER_UUID).isVoided());
-	//	}
-	//	
-	//	@Test
-	//	public void shouldPurgeOrder() throws Exception {
-	//		Assert.assertNotNull(service.getOrderByUuid(ORDER_UUID));
-	//		controller.purge(ORDER_UUID, request, response);
-	//		Assert.assertNull(service.getOrderByUuid(ORDER_UUID));
-	//	}
-	//	
-	//	@Test
-	//	public void shouldPurgeDrugOrder() throws Exception {
-	//		Assert.assertNotNull(service.getOrderByUuid(DRUG_ORDER_UUID));
-	//		controller.purge(DRUG_ORDER_UUID, request, response);
-	//		Assert.assertNull(service.getOrderByUuid(DRUG_ORDER_UUID));
-	//	}
+	private final static String PATIENT_UUID = "5946f880-b197-400b-9caa-a3c661d23041";
 	
+	private OrderService service;
+	
+	private PatientService patientService;
+			
+	@Before
+	public void before() throws Exception {
+		executeDataSet("customTestDataset.xml");
+		this.service = Context.getOrderService();
+		this.patientService = Context.getPatientService();
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.BaseCrudControllerTest#getURI()
+	 */
+	@Override
+	public String getURI() {
+		return "order";
+	}
+		
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.BaseCrudControllerTest#getAllCount()
+	 */
+	@Override
+	public long getAllCount() {
+		return service.getOrders(Order.class, null, null, null, null, null, null).size();
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.BaseCrudControllerTest#getUuid()
+	 */
+	@Override
+	public String getUuid() {
+		return ResourceTestConstants.ORDER_UUID;
+	}
+		
 	@Test
-	public void fakeTest() {
+	public void shouldGetOrderAsRef() throws Exception {
+		
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI() + "/" + getUuid());
+		req.setParameter(RestConstants.REQUEST_PROPERTY_FOR_REPRESENTATION, Representation.REF.getRepresentation());	
+		SimpleObject result = deserialize(handle(req));
+		
+		Assert.assertEquals(getUuid(), PropertyUtils.getProperty(result, "uuid"));
+		Assert.assertNotNull(PropertyUtils.getProperty(result, "display"));
+		Assert.assertNull(PropertyUtils.getProperty(result, "concept"));
+		Util.log("order as ref", result);
+	}
+		
+	@Test
+	public void shouldGetOrderAsDefault() throws Exception {
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI() + "/" + getUuid());
+		SimpleObject result = deserialize(handle(req));
+		
+		Assert.assertEquals(getUuid(), PropertyUtils.getProperty(result, "uuid"));
+		Assert.assertNotNull(PropertyUtils.getProperty(result, "display"));
+		Assert.assertNotNull(PropertyUtils.getProperty(result, "patient"));
+		Assert.assertNotNull(PropertyUtils.getProperty(result, "concept"));
+		Util.log("drug order as default", result);
+	}
+		
+	@Test
+	public void shouldGetDrugOrderAsRef() throws Exception {
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI() + "/" + DRUG_ORDER_UUID);
+		req.setParameter(RestConstants.REQUEST_PROPERTY_FOR_REPRESENTATION, Representation.REF.getRepresentation());
+		SimpleObject result = deserialize(handle(req));
+		
+		Assert.assertEquals(DRUG_ORDER_UUID, PropertyUtils.getProperty(result, "uuid"));
+		Assert.assertNotNull(PropertyUtils.getProperty(result, "display"));
+		Assert.assertNull(PropertyUtils.getProperty(result, "concept"));
+	}
+		
+	@Test
+	public void shouldGetDrugOrderAsDefault() throws Exception {
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI() + "/" + DRUG_ORDER_UUID);
+		SimpleObject result = deserialize(handle(req));
+		
+		Assert.assertEquals(DRUG_ORDER_UUID, PropertyUtils.getProperty(result, "uuid"));
+		Assert.assertNotNull(PropertyUtils.getProperty(result, "display"));
+		Assert.assertNotNull(PropertyUtils.getProperty(result, "patient"));
+		Assert.assertNotNull(PropertyUtils.getProperty(result, "concept"));
+	}
+		
+	@Test
+	public void shouldGetAllOrders() throws Exception {
+		
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+		SimpleObject result = deserialize(handle(req));
+		
+		int count = service.getOrders(Order.class, null, null, null, null, null, null).size();
+
+		Assert.assertEquals(count, Util.getResultsSize(result));
 		
 	}
+	
+	@Test
+	public void shouldGetAllDrugOrders() throws Exception {
+		
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+		req.setParameter(RestConstants.REQUEST_PROPERTY_FOR_TYPE, "drugorder");
+		SimpleObject result = deserialize(handle(req));
+		
+		int count = service.getOrders(DrugOrder.class, null, null, null, null, null, null).size();
+		
+		Assert.assertEquals(count, Util.getResultsSize(result));
+
+	}
+	
+	@Test
+	public void shouldGetAllOrdersByPatient() throws Exception {
+		
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+		req.setParameter("patient", PATIENT_UUID);		
+		SimpleObject result = deserialize(handle(req));
+		
+		Patient patient = patientService.getPatientByUuid(PATIENT_UUID);
+		List<Order> orders = service.getOrdersByPatient(patient);
+		Assert.assertEquals(orders.size(), Util.getResultsSize(result));
+
+	}
+		
+	@Test
+	public void shouldGetAllDrugOrdersByPatient() throws Exception {
+		
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+		req.setParameter(RestConstants.REQUEST_PROPERTY_FOR_TYPE, "drugorder");
+		req.setParameter("patient", PATIENT_UUID);		
+		SimpleObject result = deserialize(handle(req));
+		
+		Patient patient = patientService.getPatientByUuid(PATIENT_UUID);
+		int count = service.getDrugOrdersByPatient(patient).size();
+		
+		Assert.assertEquals(count, Util.getResultsSize(result));
+	}
+		
+	@Test
+	public void shouldCreateOrder() throws Exception {
+		
+		SimpleObject order = new SimpleObject();
+		order.add("type", "order");
+		order.add("patient", PATIENT_UUID);
+		order.add("concept", "0dde1358-7fcf-4341-a330-f119241a46e8");
+		order.add("orderType", "e23733ab-787e-4096-8ba2-577a902d2c2b");
+		
+		MockHttpServletRequest req = newPostRequest(getURI(), order);
+		SimpleObject newOrder = deserialize(handle(req));
+
+		Assert.assertEquals(order.get("type"), PropertyUtils.getProperty(newOrder, "type"));
+		Assert.assertEquals(order.get("concept"), Util.getByPath(newOrder, "concept/uuid"));
+		Assert.assertEquals(order.get("orderType"), Util.getByPath(newOrder, "orderType/uuid"));
+		Assert.assertEquals(order.get("patient"), Util.getByPath(newOrder, "patient/uuid"));
+	}	
+	
+	@Test
+	public void shouldCreateDrugOrder() throws Exception {
+		
+		SimpleObject order = new SimpleObject();
+		order.add("type", "drugorder");
+		order.add("patient", PATIENT_UUID);
+		order.add("concept", "d144d24f-6913-4b63-9660-a9108c2bebef");
+		order.add("drug", "3cfcf118-931c-46f7-8ff6-7b876f0d4202");
+		order.add("dose", "1");
+		order.add("units", "tablet");
+		
+		MockHttpServletRequest req = newPostRequest(getURI(), order);
+		SimpleObject result = deserialize(handle(req));
+		
+		Assert.assertEquals(order.get("type"), PropertyUtils.getProperty(result, "type"));
+		Assert.assertEquals(order.get("concept"), Util.getByPath(result, "concept/uuid"));
+		Assert.assertEquals(order.get("patient"), Util.getByPath(result, "patient/uuid"));
+		Assert.assertEquals(order.get("units"), PropertyUtils.getProperty(result, "units"));
+		Assert.assertEquals(order.get("drug"), Util.getByPath(result, "drug/uuid"));
+	}
+		
+	@Test
+	public void shouldUpdateOrder() throws Exception {
+		
+		SimpleObject content = new SimpleObject();
+		content.add("instructions", "Updated instructions");
+
+		MockHttpServletRequest req = newPostRequest(getURI() + "/" + getUuid(), content);
+		handle(req);
+		
+		Assert.assertEquals(content.get("instructions"), service.getOrderByUuid(getUuid()).getInstructions());
+	}
+		
+	@Test
+	public void shouldUpdateDrugOrder() throws Exception {
+		
+		SimpleObject content = new SimpleObject();
+		content.add("dose", "500");
+
+		MockHttpServletRequest req = newPostRequest(getURI() + "/" + DRUG_ORDER_UUID, content);
+		handle(req);
+		
+		DrugOrder order = (DrugOrder)service.getOrderByUuid(DRUG_ORDER_UUID);
+		Assert.assertEquals(Double.valueOf("500"), order.getDose());
+	}
+		
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailToChangeOrderType() throws Exception {
+		
+		SimpleObject content = new SimpleObject();
+		content.add("type", "order");
+
+		MockHttpServletRequest req = newPostRequest(getURI() + "/" + getUuid(), content);
+		handle(req);
+
+	}
+		
+	@Test
+	public void shouldVoidOrder() throws Exception {
+		Order order = service.getOrderByUuid(getUuid());
+		Assert.assertTrue(!order.isVoided());
+		
+		handle(newDeleteRequest(getURI() + "/" + getUuid(), new Parameter("!purge", "random reason")));
+		
+		order = service.getOrderByUuid(getUuid());
+		Assert.assertTrue(order.isVoided());
+	}
+		
+	@Test
+	public void shouldVoidDrugOrder() throws Exception {
+		Order order = service.getOrderByUuid(DRUG_ORDER_UUID);
+		Assert.assertTrue(!order.isVoided());
+		
+		handle(newDeleteRequest(getURI() + "/" + getUuid(), new Parameter("!purge", "random reason")));
+		
+		order = service.getOrderByUuid(DRUG_ORDER_UUID);
+		Assert.assertTrue(order.isVoided());
+	}
+	
+	@Test
+	public void shouldPurgeOrder() throws Exception {
+		Assert.assertNotNull(service.getOrderByUuid(getUuid()));		
+		handle(newDeleteRequest(getURI() + "/" + getUuid(), new Parameter("purge", "")));
+		Assert.assertNull(service.getOrderByUuid(getUuid()));
+	}
+	
+	@Test
+	public void shouldPurgeDrugOrder() throws Exception {
+		Assert.assertNotNull(service.getOrderByUuid(DRUG_ORDER_UUID));
+		handle(newDeleteRequest(getURI() + "/" + getUuid(), new Parameter("purge", "")));
+		Assert.assertNull(service.getOrderByUuid(DRUG_ORDER_UUID));
+	}
+
 }
