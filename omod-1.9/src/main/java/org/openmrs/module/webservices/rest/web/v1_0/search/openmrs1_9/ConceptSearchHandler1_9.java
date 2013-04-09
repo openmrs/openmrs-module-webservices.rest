9bab598d95f854aa05e0554b33d677fd4b912933
GarimaAhuja <garimaahujaishere@gmail.com>
2013-04-10 03:48:44 +0530

    RESTWS-368: Added Feature :)

---
+++ omod-1.9/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs1_9/ConceptSearchHandler1_9.java
@@ -0,0 +1,94 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.webservices.rest.web.v1_0.search.openmrs1_9;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.openmrs.Concept;
+import org.openmrs.ConceptMap;
+import org.openmrs.ConceptSource;
+import org.openmrs.ConceptReferenceTerm;
+import org.openmrs.api.ConceptService;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.api.RestHelperService;
+import org.openmrs.module.webservices.rest.web.api.RestHelperService.Field;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchConfig;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchHandler;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchQuery;
+import org.openmrs.module.webservices.rest.web.resource.impl.EmptySearchResult;
+import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * Returns concepts which map to term (uuid)
+ */
+@Component	
+public class ConceptSearchHandler1_9 implements SearchHandler {
+	
+	@Autowired
+	@Qualifier("conceptService")
+	ConceptService conceptService;
+	
+	@Autowired
+	@Qualifier("restHelperService")
+	RestHelperService restHelperService;
+	
+	private final SearchConfig searchConfig = new SearchConfig("byTerm", RestConstants.VERSION_1 + "/concept", Arrays.asList("1.8.*", "1.9.*"),
+	        new SearchQuery.Builder("Allows you to find concepts which map to term, uuid of term given as input").withRequiredParameters("uuid").build());
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.api.SearchHandler#getSearchConfig()
+	 */
+	@Override
+	public SearchConfig getSearchConfig() {
+		return searchConfig;
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.api.SearchHandler#search(org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	@Override
+	public PageableResult search(RequestContext context) throws ResponseException {
+		String uuid = context.getParameter("uuid");
+		
+		ConceptReferenceTerm conceptReferenceTerm = conceptService.getConceptReferenceTermByUuid(uuid);
+		if (conceptReferenceTerm == null) {
+			return new EmptySearchResult();
+		}
+		
+		else {
+			List<ConceptMap> conceptMaps = new ArrayList<ConceptMap>();
+			conceptMaps.addAll(restHelperService.getObjectsByFields(ConceptMap.class, new Field("conceptReferenceTerm", conceptReferenceTerm)));				
+			List<Concept> concepts = new ArrayList<Concept>();
+			for (ConceptMap conceptMap : conceptMaps) {
+				if (!conceptMap.getConcept().isRetired() || context.getIncludeAll()) {
+					concepts.add(conceptMap.getConcept());
+					
+					//the print statement was just used for the purpose of testing
+					//System.out.println(conceptMap.getConcept().getUuid());
+				}
+			}
+			return new NeedsPaging<Concept>(concepts, context);
+		}		
+	}
+	
+}
---
+++ omod-1.9/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_9/ConceptController1_9Test.java
@@ -0,0 +1,95 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.webservices.rest.web.v1_0.controller.openmrs1_9;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import java.lang.*;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openmrs.Concept;
+import org.openmrs.ConceptAnswer;
+import org.openmrs.ConceptName;
+import org.openmrs.ConceptSet;
+import org.openmrs.Drug;
+import org.openmrs.api.APIException;
+import org.openmrs.api.ConceptNameType;
+import org.openmrs.api.ConceptService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.test.Util;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.RestTestConstants1_9;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceControllerTest;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9.ConceptResource1_9;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Tests functionality of {@link ConceptController}. This does not use @should annotations because
+ * the controller inherits those methods from a subclass
+ */
+public class ConceptController1_9Test extends MainResourceControllerTest {
+
+	private ConceptService service;
+
+	@Before
+		public void before() {
+			this.service = Context.getConceptService();
+		}
+
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceControllerTest#getURI()
+	 */
+	@Override
+		public String getURI() {
+			return "concept";
+		}
+
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceControllerTest#getUuid()
+	 */
+	@Override
+		public String getUuid() {
+			return RestTestConstants1_9.CONCEPT_UUID;
+		}
+
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceControllerTest#getAllCount()
+	 */
+	@Override
+		public long getAllCount() {
+			return 24;
+		}
+
+
+	@Test
+		public void shouldFindConceptsBySourceUuid() throws Exception {
+			SimpleObject response = deserialize(handle(newGetRequest(getURI(), new Parameter("uuid",
+								"SSTRM-WGT234"))));
+			List<Object> results = Util.getResultsList(response);
+
+			Assert.assertEquals(results.size(), 1);
+			Object next = results.iterator().next();
+			Assert.assertThat((String) PropertyUtils.getProperty(next, "uuid"), is("c607c80f-1ea9-4da3-bb88-6276ce8868dd"));
+		}
+}
