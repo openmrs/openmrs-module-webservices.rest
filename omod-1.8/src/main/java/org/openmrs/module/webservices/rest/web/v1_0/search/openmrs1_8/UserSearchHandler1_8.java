89bd8cdc37718c4c6659d572b110c8781d5d4e25
draggonz <dragggonz@gmail.com>
2013-04-13 10:22:04 +0600

    RESTWS-375: Find User by username

---
+++ omod-1.8/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs1_8/UserSearchHandler1_8.java
@@ -0,0 +1,68 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.webservices.rest.web.v1_0.search.openmrs1_8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.openmrs.User;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchConfig;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchHandler;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchQuery;
+import org.openmrs.module.webservices.rest.web.resource.impl.EmptySearchResult;
+import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+import org.springframework.stereotype.Component;
+
+/**
+ * Allows for finding users by mapping
+ */
+@Component
+public class UserSearchHandler1_8 implements SearchHandler {
+	
+	private final SearchConfig searchConfig = new SearchConfig("default", RestConstants.VERSION_1 + "/user",
+	        Arrays.asList("1.8.*", "1.9.*"), new SearchQuery.Builder("Allows you to find users by username")
+	                .withRequiredParameters("username").build());
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.api.SearchHandler#getSearchConfig()
+	 */
+	@Override
+	public SearchConfig getSearchConfig() {
+		return searchConfig;
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.api.SearchHandler#search(org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	@Override
+	public PageableResult search(RequestContext context) throws ResponseException {
+		String username = context.getParameter("username");
+		
+		User user = Context.getUserService().getUserByUsername(username);
+		if (user == null){
+			return new EmptySearchResult();
+		}
+		
+		List<User> users = new ArrayList<User>();
+		users.add(user);
+		return new NeedsPaging<User>(users, context);
+	}
+	
+}
