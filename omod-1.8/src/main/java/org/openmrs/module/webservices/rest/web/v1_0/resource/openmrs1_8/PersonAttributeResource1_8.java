afbdf50bdff0a8e8fa48bbc01fd9e0ca284e1860
Shruthi <shruthidipali@gmail.com>
2013-10-01 20:04:53 +0530

    RESTWS-389 PersonAttributeResource rest api includes hydratedObject property which takes uuid/object representation of attributables

--- omod-1.8/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_8/PersonAttributeResource1_8.java
+++ omod-1.8/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_8/PersonAttributeResource1_8.java
@@ -13,11 +13,17 @@
  */
 package org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8;
 
+import org.openmrs.Attributable;
+import org.openmrs.Concept;
 import org.openmrs.Person;
 import org.openmrs.PersonAttribute;
+import org.openmrs.api.APIException;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.ConversionUtil;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.PropertySetter;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.annotation.SubResource;
 import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
@@ -27,15 +33,14 @@ import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceD
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingSubResource;
 import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
-import org.openmrs.module.webservices.rest.SimpleObject;
-import org.openmrs.module.webservices.rest.web.ConversionUtil;
+import org.openmrs.util.OpenmrsClassLoader;
 
 /**
  * {@link Resource} for PersonAttributes, supporting standard CRUD operations
  */
 @SubResource(parent = PersonResource1_8.class, path = "attribute", supportedClass = PersonAttribute.class, supportedOpenmrsVersions = {"1.8.*", "1.9.*"})
 public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttribute, Person, PersonResource1_8> {
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getRepresentationDescription(org.openmrs.module.webservices.rest.web.representation.Representation)
 	 */
@@ -56,6 +61,7 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 			description.addProperty("display", findMethod("getDisplayString"));
 			description.addProperty("uuid");
 			description.addProperty("value", findMethod("getValue"));
+            description.addProperty("hydratedObject");
 			description.addProperty("attributeType", Representation.REF);
 			description.addProperty("voided");
 			description.addProperty("auditInfo", findMethod("getAuditInfo"));
@@ -64,14 +70,46 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 		}
 		return null;
 	}
-	
+
 	public DelegatingResourceDescription getCreatableProperties() {
 		DelegatingResourceDescription description = new DelegatingResourceDescription();
-		description.addRequiredProperty("value");
+		description.addProperty("value");
+        description.addProperty("hydratedObject");
 		description.addRequiredProperty("attributeType");
 		return description;
 	}
-	
+
+    /*
+        We may need to hydrate Attributables in the request. To do that we need to know
+        its type. So we can't set hydratedObject until attributeType has been set.
+        Since SimpleObject is a LinkedHashMap, it is ordered, and the following method is
+        overridden to ensure hydratedObject will appear last in the properties.
+     */
+    @Override
+    public Object create(String parentUniqueId, SimpleObject post, RequestContext context) throws ResponseException {
+        uglyMethodToEnsureHydratedObjectWillBeSetLast(post);
+        return super.create(parentUniqueId, post, context);
+    }
+
+    private void uglyMethodToEnsureHydratedObjectWillBeSetLast(SimpleObject post) {
+        Object hydratedObject = post.get("hydratedObject");
+        if (hydratedObject != null) {
+            post.remove("hydratedObject");
+            post.put("hydratedObject", hydratedObject);
+        }
+    }
+
+    @PropertySetter("hydratedObject")
+    public void setHydratedObject(PersonAttribute personAttribute, String attributableUuid) {
+        try {
+            Class<?> attributableClass = OpenmrsClassLoader.getInstance().loadClass(personAttribute.getAttributeType().getFormat());
+            Attributable value = (Attributable) ConversionUtil.convert(attributableUuid, attributableClass);
+            personAttribute.setValue(value.serialize());
+        } catch (ClassNotFoundException e) {
+            throw new APIException("Could not convert value to Attributable", e);
+        }
+    }
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getUpdatableProperties()
 	 */
@@ -79,7 +117,7 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 	public DelegatingResourceDescription getUpdatableProperties() {
 		return getCreatableProperties();
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingSubResource#getParent(java.lang.Object)
 	 */
@@ -87,7 +125,7 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 	public Person getParent(PersonAttribute instance) {
 		return instance.getPerson();
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#newDelegate()
 	 */
@@ -95,7 +133,7 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 	public PersonAttribute newDelegate() {
 		return new PersonAttribute();
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingSubResource#setParent(java.lang.Object,
 	 *      java.lang.Object)
@@ -104,7 +142,7 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 	public void setParent(PersonAttribute instance, Person person) {
 		instance.setPerson(person);
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getByUniqueId(java.lang.String)
 	 */
@@ -112,7 +150,7 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 	public PersonAttribute getByUniqueId(String uniqueId) {
 		return Context.getPersonService().getPersonAttributeByUuid(uniqueId);
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingSubResource#doGetAll(java.lang.Object,
 	 *      org.openmrs.module.webservices.rest.web.RequestContext)
@@ -121,7 +159,7 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 	public NeedsPaging<PersonAttribute> doGetAll(Person parent, RequestContext context) throws ResponseException {
 		return new NeedsPaging<PersonAttribute>(parent.getActiveAttributes(), context);
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceHandler#save(java.lang.Object)
 	 */
@@ -137,12 +175,12 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 		}
 		if (needToAdd)
 			delegate.getPerson().addAttribute(delegate);
-		
+
 		Context.getPersonService().savePerson(delegate.getPerson());
-		
+
 		return delegate;
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#delete(java.lang.Object,
 	 *      java.lang.String, org.openmrs.module.webservices.rest.web.RequestContext)
@@ -152,7 +190,7 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 		delegate.voidAttribute(reason);
 		Context.getPersonService().savePerson(delegate.getPerson());
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#purge(java.lang.Object,
 	 *      org.openmrs.module.webservices.rest.web.RequestContext)
@@ -162,23 +200,26 @@ public class PersonAttributeResource1_8 extends DelegatingSubResource<PersonAttr
 		delegate.getPerson().removeAttribute(delegate);
 		Context.getPersonService().savePerson(delegate.getPerson());
 	}
-	
+
 	/**
 	 * Gets the display string for a person attribute.
-	 * 
+	 *
 	 * @param pa the person attribute.
 	 * @return attribute type + value (for concise display purposes)
 	 */
 	public String getDisplayString(PersonAttribute pa) {
 		if (pa.getAttributeType() == null)
 			return "";
-		
-		return pa.getAttributeType().getName() + " = " + pa.getValue();
-	}
+        if (Concept.class.getName().equals(pa.getAttributeType().getFormat()) && pa.getValue() != null) {
+            Concept concept = Context.getConceptService().getConcept(pa.getValue());
+            return concept == null ? null : concept.getDisplayString();
+        }
+        return pa.getAttributeType().getName() + " = " + pa.getValue();
+    }
 
 	/**
 	 * Gets the hydrated object of person attribute.
-	 * 
+	 *
 	 * @param pa the person attribute.
 	 * @return an object containing the hydrated object.
 	 */
