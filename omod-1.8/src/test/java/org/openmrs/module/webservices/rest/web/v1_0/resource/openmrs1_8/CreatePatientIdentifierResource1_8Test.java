4b07e25fb20aea8e233a2bfd1cc21c3fc32d63ab
Shruthi <shruthidipali@gmail.com>
2013-10-30 15:30:29 +0530

    Adding the option to set the PatientIdentifierType by name.

--- omod-1.8/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_8/CreatePatientIdentifierResource1_8Test.java
+++ omod-1.8/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_8/CreatePatientIdentifierResource1_8Test.java
@@ -14,29 +14,51 @@ import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
 import java.util.HashMap;
 
 public class CreatePatientIdentifierResource1_8Test extends BaseModuleWebContextSensitiveTest {
-    public static final String PERSON_ATTRIBUTE_JSON = "{" +
-            "            \"identifier\": \"OpenMRS-ID-123\"," +
-            "            \"identifierType\": {" +
-            "              \"uuid\" : \"1a339fe9-38bc-4ab3-b180-320988c0b968\"" +
-            "            }," +
-            "            \"location\" : {" +
-            "              \"uuid\" : \"dc5c1fcc-0459-4201-bf70-0b90535ba362\"" +
-            "            }," +
-            "            \"preferred\": true" +
-            "        }";
-
-    private SimpleObject personAttributeSimpleObject = new SimpleObject();
-
+    
     private PatientIdentifierResource1_8 resource;
 
     @Before
     public void beforeEachTests() throws Exception {
-        personAttributeSimpleObject.putAll(new ObjectMapper().readValue(PERSON_ATTRIBUTE_JSON, HashMap.class));
         resource = (PatientIdentifierResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(PatientIdentifier.class);
     }
 
     @Test
-    public void shouldCreatePatientIdentifier() throws Exception {
+    public void shouldCreatePatientIdentifier_WhenTypeIsSpecifiedByUuid() throws Exception {
+        
+         String personAttributeJson = "{" +
+                "            \"identifier\": \"OpenMRS-ID-123\"," +
+                "            \"identifierType\": {" +
+                "              \"uuid\" : \"1a339fe9-38bc-4ab3-b180-320988c0b968\"" +
+                "            }," +
+                "            \"location\" : {" +
+                "              \"uuid\" : \"dc5c1fcc-0459-4201-bf70-0b90535ba362\"" +
+                "            }," +
+                "            \"preferred\": true" +
+                "        }";
+
+        SimpleObject personAttributeSimpleObject = new SimpleObject();
+        personAttributeSimpleObject.putAll(new ObjectMapper().readValue(personAttributeJson, HashMap.class));
+
+        SimpleObject created = (SimpleObject) resource.create("da7f524f-27ce-4bb2-86d6-6d1d05312bd5", personAttributeSimpleObject, new RequestContext());
+        Assert.assertEquals("OpenMRS-ID-123", created.get("identifier"));
+    }
+
+    @Test
+    public void shouldCreatePatientIdentifier_WhenTypeIsSpecifiedByName() throws Exception {
+        String personAttributeJson = "{" +
+                "            \"identifier\": \"OpenMRS-ID-123\"," +
+                "            \"identifierType\": {" +
+                "              \"name\" : \"OpenMRS Identification Number\"" +
+                "            }," +
+                "            \"location\" : {" +
+                "              \"uuid\" : \"dc5c1fcc-0459-4201-bf70-0b90535ba362\"" +
+                "            }," +
+                "            \"preferred\": true" +
+                "        }";
+
+        SimpleObject personAttributeSimpleObject = new SimpleObject();
+        personAttributeSimpleObject.putAll(new ObjectMapper().readValue(personAttributeJson, HashMap.class));
+
         SimpleObject created = (SimpleObject) resource.create("da7f524f-27ce-4bb2-86d6-6d1d05312bd5", personAttributeSimpleObject, new RequestContext());
         Assert.assertEquals("OpenMRS-ID-123", created.get("identifier"));
     }
