23d3793225cb411cc9b0dd1d4c71dce5fcf8a677
wyclif <wyclif@openmrs.org>
2014-02-13 17:20:06 -0500

    Follow up to clean up CareSettingResource1_10, CareSettingResource1_10Test and added unit tests for care setting controller - RESTWS-413

--- omod-1.10/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_10/CareSettingResource1_10.java
+++ omod-1.10/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_10/CareSettingResource1_10.java
@@ -12,6 +12,10 @@
  */
 package org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_10;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import org.openmrs.CareSetting;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.web.RequestContext;
@@ -27,93 +31,112 @@ import org.openmrs.module.webservices.rest.web.response.ResourceDoesNotSupportOp
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 
 /**
- * {@link org.openmrs.module.webservices.rest.web.annotation.Resource} for {@link org.openmrs.CareSetting}, supporting standard CRUD operations
+ * {@link org.openmrs.module.webservices.rest.web.annotation.Resource} for
+ * {@link org.openmrs.CareSetting}, supporting standard CRUD operations
  */
 @Resource(name = RestConstants.VERSION_1 + "/caresetting", supportedClass = CareSetting.class, supportedOpenmrsVersions = "1.10.*")
 public class CareSettingResource1_10 extends MetadataDelegatingCrudResource<CareSetting> {
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#getRepresentationDescription(org.openmrs.module.webservices.rest.web.representation.Representation)
-     */
-    @Override
-    public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
-        if (rep instanceof DefaultRepresentation || rep instanceof FullRepresentation) {
-            DelegatingResourceDescription description = new DelegatingResourceDescription();
-            description.addProperty("uuid");
-            description.addProperty("name");
-            description.addProperty("description");
-            description.addProperty("retired");
-            description.addProperty("careSettingType");
-            description.addSelfLink();
-            return description;
-        }
-        return null;
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getCreatableProperties()
-     */
-    @Override
-    public DelegatingResourceDescription getCreatableProperties() {
-        return new DelegatingResourceDescription();
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#newDelegate()
-     */
-    @Override
-    public CareSetting newDelegate() {
-        return new CareSetting();
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#save(Object)
-     */
-    @Override
-    public CareSetting save(CareSetting careSetting) {
-        throw new ResourceDoesNotSupportOperationException();
-    }
-
-    /**
-     * Fetches a careSettings by id, if no match is found.
-     *
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#getByUniqueId(String)
-     */
-    @Override
-    public CareSetting getByUniqueId(String id) {
-        return Context.getOrderService().getCareSetting(Integer.parseInt(id));
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#purge(Object,
-     *      org.openmrs.module.webservices.rest.web.RequestContext)
-     */
-    @Override
-    public void purge(CareSetting careSetting, RequestContext context) throws ResponseException {
-        throw new ResourceDoesNotSupportOperationException();
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#doGetAll(org.openmrs.module.webservices.rest.web.RequestContext)
-     */
-    @Override
-    protected NeedsPaging<CareSetting> doGetAll(RequestContext context) {
-        throw new ResourceDoesNotSupportOperationException();
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#doSearch(org.openmrs.module.webservices.rest.web.RequestContext)
-     */
-    @Override
-    protected NeedsPaging<CareSetting> doSearch(RequestContext context) {
-        throw new ResourceDoesNotSupportOperationException();
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getResourceVersion()
-     */
-    @Override
-    public String getResourceVersion() {
-        return "1.10";
-    }
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#getRepresentationDescription(org.openmrs.module.webservices.rest.web.representation.Representation)
+	 */
+	@Override
+	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
+		if (rep instanceof DefaultRepresentation || rep instanceof FullRepresentation) {
+			DelegatingResourceDescription description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("name");
+			description.addProperty("description");
+			description.addProperty("retired");
+			description.addProperty("careSettingType");
+			description.addProperty("display", findMethod("getDisplayString"));
+			description.addSelfLink();
+			if (rep instanceof DefaultRepresentation) {
+				description.addLink("full", ".?v=" + RestConstants.REPRESENTATION_FULL);
+			} else {
+				description.addProperty("auditInfo", findMethod("getAuditInfo"));
+			}
+			return description;
+		}
+		return null;
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getCreatableProperties()
+	 */
+	@Override
+	public DelegatingResourceDescription getCreatableProperties() {
+		throw new ResourceDoesNotSupportOperationException();
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#newDelegate()
+	 */
+	@Override
+	public CareSetting newDelegate() {
+		return new CareSetting();
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#save(Object)
+	 */
+	@Override
+	public CareSetting save(CareSetting careSetting) {
+		throw new ResourceDoesNotSupportOperationException();
+	}
+	
+	/**
+	 * Fetches a careSettings by uuid or name
+	 * 
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#getByUniqueId(String)
+	 */
+	@Override
+	public CareSetting getByUniqueId(String uniqueId) {
+		CareSetting cs = Context.getOrderService().getCareSettingByUuid(uniqueId);
+		if (cs == null) {
+			cs = Context.getOrderService().getCareSettingByName(uniqueId);
+		}
+		return cs;
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#purge(Object,
+	 *      org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	@Override
+	public void purge(CareSetting careSetting, RequestContext context) throws ResponseException {
+		throw new ResourceDoesNotSupportOperationException();
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#doGetAll(org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	@Override
+	protected NeedsPaging<CareSetting> doGetAll(RequestContext context) {
+		return new NeedsPaging<CareSetting>(Context.getOrderService().getCareSettings(context.getIncludeAll()), context);
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#doSearch(org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	@Override
+	protected NeedsPaging<CareSetting> doSearch(RequestContext context) {
+		List<CareSetting> careSettings = Context.getOrderService().getCareSettings(context.getIncludeAll());
+		for (Iterator<CareSetting> iterator = careSettings.iterator(); iterator.hasNext();) {
+			CareSetting cs = iterator.next();
+			if (!Pattern.compile(Pattern.quote(context.getParameter("q")), Pattern.CASE_INSENSITIVE).matcher(cs.getName())
+			        .find()) {
+				iterator.remove();
+			}
+		}
+		return new NeedsPaging<CareSetting>(careSettings, context);
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getResourceVersion()
+	 */
+	@Override
+	public String getResourceVersion() {
+		return "1.10";
+	}
 }
---
+++ omod-1.10/src/test/java/org/openmrs/module/webservices/rest/web/RestTestConstants1_10.java
@@ -0,0 +1,18 @@
+/**
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.webservices.rest.web;
+
+public class RestTestConstants1_10 {
+	
+	public final static String CARE_SETTING_UUID = "2ed1e57d-9f18-41d3-b067-2eeaf4b30fb1";
+}
