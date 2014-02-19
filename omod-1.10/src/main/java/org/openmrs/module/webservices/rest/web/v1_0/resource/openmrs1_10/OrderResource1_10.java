c1531c7482f889d524254709a55ace2fc32151ea
wyclif <wyclif@openmrs.org>
2014-02-18 23:35:12 -0500

    Follow up to clean up OrderResource1_10

--- omod-1.10/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_10/OrderResource1_10.java
+++ omod-1.10/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_10/OrderResource1_10.java
@@ -13,11 +13,8 @@
 package org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_10;
 
 import org.openmrs.Order;
-import org.openmrs.activelist.Allergy;
-import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
-import org.openmrs.module.webservices.rest.web.annotation.PropertySetter;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
@@ -29,137 +26,107 @@ import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.OrderResource1_8;
 
 /**
- * {@link org.openmrs.module.webservices.rest.web.annotation.Resource} for {@link org.openmrs.Order}, supporting standard CRUD operations
+ * {@link org.openmrs.module.webservices.rest.web.annotation.Resource} for {@link org.openmrs.Order}
+ * , supporting standard CRUD operations
  */
 @Resource(name = RestConstants.VERSION_1 + "/order", supportedClass = Order.class, supportedOpenmrsVersions = "1.10.*")
 public class OrderResource1_10 extends OrderResource1_8 {
-
-    @Override
-    public boolean hasTypesDefined() {
-        return false;
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#getRepresentationDescription(org.openmrs.module.webservices.rest.web.representation.Representation)
-     */
-    @Override
-    public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
-        if (rep instanceof DefaultRepresentation) {
-            DelegatingResourceDescription description = new DelegatingResourceDescription();
-            description.addProperty("uuid");
-            description.addProperty("display", findMethod("getDisplayString"));
-            description.addProperty("patient", Representation.REF);
-            description.addProperty("concept", Representation.REF);
-            description.addProperty("instructions");
-            description.addProperty("startDate");
-            description.addProperty("autoExpireDate");
-            description.addProperty("encounter", Representation.REF);
-            description.addProperty("orderer", Representation.REF);
-            description.addProperty("action");
-            description.addProperty("accessionNumber");
-            description.addProperty("orderReason");
-            description.addProperty("orderReasonNonCoded");
-            description.addProperty("urgency");
-            description.addProperty("orderNumber");
-            description.addProperty("commentToFulfiller");
-            description.addProperty("careSetting", Representation.REF);
-            description.addProperty("previousOrder", Representation.REF);
-            description.addSelfLink();
-            description.addLink("full", ".?v=" + RestConstants.REPRESENTATION_FULL);
-            return description;
-        } else if (rep instanceof FullRepresentation) {
-            DelegatingResourceDescription description = new DelegatingResourceDescription();
-            description.addProperty("uuid");
-            description.addProperty("display", findMethod("getDisplayString"));
-            description.addProperty("patient", Representation.REF);
-            description.addProperty("concept", Representation.REF);
-            description.addProperty("instructions");
-            description.addProperty("startDate");
-            description.addProperty("autoExpireDate");
-            description.addProperty("encounter", Representation.REF);
-            description.addProperty("orderer", Representation.REF);
-            description.addProperty("action");
-            description.addProperty("accessionNumber");
-            description.addProperty("orderReason");
-            description.addProperty("orderReasonNonCoded");
-            description.addProperty("urgency");
-            description.addProperty("orderNumber");
-            description.addProperty("commentToFulfiller");
-            description.addProperty("careSetting", Representation.FULL);
-            description.addProperty("previousOrder", Representation.FULL);
-            description.addSelfLink();
-            return description;
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getCreatableProperties()
-     */
-    @Override
-    public DelegatingResourceDescription getCreatableProperties() {
-        DelegatingResourceDescription d = new DelegatingResourceDescription();
-        d.addRequiredProperty("patient");
-        d.addRequiredProperty("concept");
-        d.addRequiredProperty("action");
-        d.addRequiredProperty("startDate");
-        d.addProperty("previousOrder");
-        return d;
-    }
-
-    @Override
-    protected PageableResult doGetAll(RequestContext context) throws ResponseException {
-        throw new ResourceDoesNotSupportOperationException();
-    }
-    
-    /**
-     * Annotated setter for previousOrder
-     *
-     * @param order
-     * @param value uuid of the previous order
-     */
-    @PropertySetter("previousOrder")
-    public static void setPreviousOrder(Order order, Object value) {
-        order.setPreviousOrder(Context.getOrderService().getOrderByUuid((String) value));
-    }
-
-    /**
-     * Annotated setter for patient
-     *
-     * @param order
-     * @param value uuid of the patient
-     */
-    @PropertySetter("patient")
-    public static void setPatient(Order order, Object value) {
-        order.setPatient(Context.getPatientService().getPatientByUuid((String) value));
-    }
-
-    /**
-     * Annotated setter for concept
-     *
-     * @param order
-     * @param value uuid of the concept
-     */
-    @PropertySetter("concept")
-    public static void setConcept(Order order, Object value) {
-        order.setConcept(Context.getConceptService().getConceptByUuid((String) value));
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#purge(Object,
-     *      org.openmrs.module.webservices.rest.web.RequestContext)
-     */
-    @Override
-    public void purge(Order order, RequestContext context) throws ResponseException {
-        throw new ResourceDoesNotSupportOperationException();
-    }
-
-    /**
-     * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getResourceVersion()
-     */
-    @Override
-    public String getResourceVersion() {
-        return "1.10";
-    }
+	
+	@Override
+	public boolean hasTypesDefined() {
+		return true;
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#getRepresentationDescription(org.openmrs.module.webservices.rest.web.representation.Representation)
+	 */
+	@Override
+	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
+		if (rep instanceof DefaultRepresentation) {
+			DelegatingResourceDescription description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("orderNumber");
+			description.addProperty("patient", Representation.REF);
+			description.addProperty("concept", Representation.REF);
+			description.addProperty("action");
+			description.addProperty("careSetting", Representation.REF);
+			description.addProperty("previousOrder", Representation.REF);
+			description.addProperty("startDate");
+			description.addProperty("dateStopped");
+			description.addProperty("autoExpireDate");
+			description.addProperty("encounter", Representation.REF);
+			description.addProperty("orderer", Representation.REF);
+			description.addProperty("orderReason", Representation.REF);
+			description.addProperty("orderReasonNonCoded");
+			description.addProperty("urgency");
+			description.addProperty("instructions");
+			description.addProperty("commentToFulfiller");
+			description.addProperty("display", findMethod("getDisplayString"));
+			description.addSelfLink();
+			description.addLink("full", ".?v=" + RestConstants.REPRESENTATION_FULL);
+			return description;
+		} else if (rep instanceof FullRepresentation) {
+			DelegatingResourceDescription description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("orderNumber");
+			description.addProperty("patient", Representation.REF);
+			description.addProperty("concept", Representation.REF);
+			description.addProperty("action");
+			description.addProperty("careSetting", Representation.DEFAULT);
+			description.addProperty("previousOrder", Representation.REF);
+			description.addProperty("startDate");
+			description.addProperty("dateStopped");
+			description.addProperty("autoExpireDate");
+			description.addProperty("encounter", Representation.REF);
+			description.addProperty("orderer", Representation.REF);
+			description.addProperty("orderReason", Representation.REF);
+			description.addProperty("orderReasonNonCoded");
+			description.addProperty("urgency");
+			description.addProperty("instructions");
+			description.addProperty("commentToFulfiller");
+			description.addProperty("display", findMethod("getDisplayString"));
+			description.addProperty("auditInfo", findMethod("getAuditInfo"));
+			description.addSelfLink();
+			return description;
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getCreatableProperties()
+	 */
+	@Override
+	public DelegatingResourceDescription getCreatableProperties() {
+		DelegatingResourceDescription d = new DelegatingResourceDescription();
+		d.addRequiredProperty("patient");
+		d.addRequiredProperty("concept");
+		d.addRequiredProperty("action");
+		d.addRequiredProperty("startDate");
+		d.addRequiredProperty("careSetting");
+		d.addRequiredProperty("encounter");
+		d.addProperty("dateStopped");
+		d.addProperty("autoExpireDate");
+		d.addProperty("orderer");
+		d.addProperty("previousOrder");
+		d.addProperty("urgency");
+		d.addProperty("orderReason");
+		d.addProperty("orderReasonNonCoded");
+		d.addProperty("instructions");
+		d.addProperty("commentToFulfiller");
+		return d;
+	}
+	
+	@Override
+	protected PageableResult doGetAll(RequestContext context) throws ResponseException {
+		throw new ResourceDoesNotSupportOperationException();
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getResourceVersion()
+	 */
+	@Override
+	public String getResourceVersion() {
+		return RestConstants1_10.RESOURCE_VERSION;
+	}
 }
