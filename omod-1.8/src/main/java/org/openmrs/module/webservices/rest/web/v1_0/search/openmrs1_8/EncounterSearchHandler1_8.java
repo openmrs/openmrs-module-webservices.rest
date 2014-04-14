e968fcd0019664e0ff48ce19a731e9ba19537cd4
sushmitharaos <sushmitharaos@gmail.com>
2014-04-14 11:25:26 +0530

    RESTWS-430 | feedback on the pull request encorporated

--- omod-1.8/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs1_8/EncounterSearchHandler1_8.java
+++ omod-1.8/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs1_8/EncounterSearchHandler1_8.java
@@ -1,3 +1,17 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ * <p/>
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * <p/>
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
 package org.openmrs.module.webservices.rest.web.v1_0.search.openmrs1_8;
 
 import org.openmrs.Encounter;
@@ -21,19 +35,6 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
- * <p/>
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- * <p/>
- * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
- */
 @Component
 public class EncounterSearchHandler1_8 implements SearchHandler {
     private final SearchConfig searchConfig = new SearchConfig("default", RestConstants.VERSION_1 + "/encounter", Arrays.asList("1.8.*", "1.9.*", "1.10.*"),
@@ -48,18 +49,15 @@ public class EncounterSearchHandler1_8 implements SearchHandler {
     public PageableResult search(RequestContext context) throws ResponseException {
         String patientUuid = context.getRequest().getParameter("patient");
         String encounterTypeUuid = context.getRequest().getParameter("encounterType");
-        if (patientUuid != null) {
-            Patient patient = ((PatientResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(
-                    Patient.class)).getByUniqueId(patientUuid);
-            EncounterType encounterType = ((EncounterTypeResource1_8)
-                    Context.getService(RestService.class).getResourceBySupportedClass(EncounterType.class)).getByUniqueId(encounterTypeUuid);
-            if (patient != null && encounterType != null) {
-                List<Encounter> encounters = Context.getEncounterService()
-                        .getEncounters(patient, null, null, null, null, Arrays.asList(encounterType), null, false);
-                return new NeedsPaging<Encounter>(encounters, context);
-            }
+        Patient patient = ((PatientResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(
+                Patient.class)).getByUniqueId(patientUuid);
+        EncounterType encounterType = ((EncounterTypeResource1_8)
+                Context.getService(RestService.class).getResourceBySupportedClass(EncounterType.class)).getByUniqueId(encounterTypeUuid);
+        if (patient != null && encounterType != null) {
+            List<Encounter> encounters = Context.getEncounterService()
+                    .getEncounters(patient, null, null, null, null, Arrays.asList(encounterType), null, false);
+            return new NeedsPaging<Encounter>(encounters, context);
         }
-
         return new EmptySearchResult();
     }
 
--- omod-1.8/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/EncounterController1_8Test.java
+++ omod-1.8/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/EncounterController1_8Test.java
@@ -276,7 +276,11 @@ public class EncounterController1_8Test extends MainResourceControllerTest {
 
         SimpleObject result = deserialize(handle(newGetRequest(getURI(), new Parameter("s","default"),new Parameter("patient", "41c6b35e-c093-11e3-be87-005056821db0"), new Parameter("encounterType", "ff7397ea-c090-11e3-be87-005056821db0"))));
 
-        Assert.assertEquals(1,((ArrayList)result.get("results")).size());
+
+        ArrayList encounters = (ArrayList) result.get("results");
+        Assert.assertEquals(1, encounters.size());
+        String encounterUuid = (String) PropertyUtils.getProperty(encounters.get(0), "uuid");
+        Assert.assertEquals("62967e68-96bb-11e0-8d6b-9b9415a91465", encounterUuid);
     }
 	
 	/**
