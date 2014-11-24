f395be5049e772f6fa0db5f005bf7be62a577a5f
Lluis Martinez <lluismf@gmail.com>
2014-11-24 23:25:22 +0100

    RESTWS-468: Support referring to concepts by SAME-AS mapping

--- omod-1.9/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_9/ConceptResource1_9.java
+++ omod-1.9/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_9/ConceptResource1_9.java
@@ -14,6 +14,7 @@
 package org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9;
 
 import org.openmrs.Concept;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.ConceptResource1_8;
@@ -33,4 +34,19 @@ public class ConceptResource1_9 extends ConceptResource1_8 {
 	    return RestConstants1_9.RESOURCE_VERSION;
 	}
 	
+	public Concept getByUniqueId(String identifier) {
+		
+		Concept concept = null;
+
+		if (identifier.contains(":")) {
+			String[] tokens = identifier.split(":");
+			String sourceName = tokens[0];
+			String termCode = tokens[1];
+			concept = Context.getConceptService().getConceptByMapping(termCode, sourceName, true);
+		} else {
+			concept = Context.getConceptService().getConceptByUuid(identifier);
+		}
+
+		return concept;
+	}
 }
