2d9a746cf8d6223e9ddd2079726cdc78a79eab71
Lluis Martinez Ferrando <lluismf@gmail.com>
2015-04-02 22:28:09 +0200

    Revert change

--- omod-1.8/src/test/java/org/openmrs/module/webservices/rest/web/ClassHierarchyResourceTest.java
+++ omod-1.8/src/test/java/org/openmrs/module/webservices/rest/web/ClassHierarchyResourceTest.java
@@ -13,8 +13,6 @@
  */
 package org.openmrs.module.webservices.rest.web;
 
-import java.util.List;
-
 import org.apache.commons.beanutils.PropertyUtils;
 import org.junit.Assert;
 import org.junit.Before;
@@ -22,8 +20,6 @@ import org.junit.Test;
 import org.openmrs.DrugOrder;
 import org.openmrs.Order;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.webservices.docs.ResourceDoc;
-import org.openmrs.module.webservices.docs.ResourceDocCreator;
 import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.test.Util;
 import org.openmrs.module.webservices.rest.web.api.RestService;
@@ -70,44 +66,6 @@ public class ClassHierarchyResourceTest extends BaseModuleWebContextSensitiveTes
 		return buildSuperclass().removeProperty("orderType").add(RestConstants.PROPERTY_FOR_TYPE, "drugorder").add("dose",
 		    "100").add("units", "mg").add("prn", "true").add("complex", "false").add("drug", ASPIRIN_DRUG_UUID);
 	}
-	
-	@Test
-	public void createDocumentation() throws Exception {
-		
-		List<ResourceDoc> docs = ResourceDocCreator.create("/ws");
-		
-		String header = "This page is generated automatically and must not be edited manually. It describes the urls/resources published by the [docs:Webservices.rest Module].";
-		header += System.getProperty("line.separator");
-		header += System.getProperty("line.separator");
-		header += "For more information on conventions, authentication, and setup, see the main [docs:REST Web Services API] page.";
-		header += System.getProperty("line.separator");
-		header += System.getProperty("line.separator");
-		header += "{toc:maxLevel=1}";
-		header += System.getProperty("line.separator");
-		header += "{note}";
-		header += System.getProperty("line.separator");
-		header += System.getProperty("line.separator");
-		header += "When a parameter has a bang \"!\" in front of it, it means that url will only be called if that parameter does NOT exist.\n\n";
-		header += "Properties listed in *bold* must not be null.";
-		header += System.getProperty("line.separator");
-		header += System.getProperty("line.separator");
-		header += "{note}";
-		header += System.getProperty("line.separator");
-		
-		System.out.println(header);
-		
-		for (ResourceDoc doc : docs) {
-			
-			String text = doc.toString();
-			
-			//Wiki syntax will think these are macros and complain like: Unknown macro: {......}
-			text = text.replace("{", "\\{");
-			text = text.replace("}", "\\}");
-			
-			System.out.println(text);
-		}
-	}
-	
 	
 	@Test
 	public void shouldCreateASuperclass() throws Exception {
