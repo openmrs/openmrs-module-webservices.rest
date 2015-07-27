95e9629175ae80722a1864f184ad8c7a318aa061
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-07-27 14:07:36 +0200

    RESTWS-515 Delete patient should void visits

---
+++ omod-1.9/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_9/PatientResource1_9.java
@@ -0,0 +1,46 @@
+/**
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9;
+
+import java.util.List;
+
+import org.openmrs.Patient;
+import org.openmrs.Visit;
+import org.openmrs.api.VisitService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.PatientResource1_8;
+
+@Resource(name = RestConstants.VERSION_1 + "/patient", supportedClass = Patient.class, supportedOpenmrsVersions = {"1.9.*", "1.10.*", "1.11.*", "1.12.*"})
+public class PatientResource1_9 extends PatientResource1_8 {
+	
+	@Override
+	public void delete(Patient patient, String reason, RequestContext context) throws ResponseException {
+		if (patient.isVoided()) {
+			// DELETE is idempotent, so we return success here
+			return;
+		}
+		
+		VisitService visitService = Context.getVisitService();
+	    List<Visit> visits = Context.getVisitService().getVisitsByPatient(patient);
+	    for (Visit visit : visits) {
+	        visitService.voidVisit(visit, "Patient deleted");
+        }
+	    
+	    super.delete(patient, reason, context);
+	}
+	
+}
