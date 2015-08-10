bbc23d7bca1717f8884d62f5b9e3f01f77274c0e
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-08-10 16:53:50 +0200

    RA-519 Add CustomDatatype representation

---
+++ omod-1.9/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_9/CustomDatatypeRepresentation.java
@@ -0,0 +1,44 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CustomDatatypeRepresentation {
+	
+	private String uuid;
+	
+	private String datatypeClassname;
+	
+	private List<String> handlerClassnames = new ArrayList<String>();
+	
+    public String getUuid() {
+	    return uuid;
+    }
+	
+	public String getDatatypeClassname() {
+		return datatypeClassname;
+	}
+	
+	public void setDatatypeClassname(String datatypeClass) {
+		this.datatypeClassname = datatypeClass;
+		this.uuid = UUID.nameUUIDFromBytes(datatypeClassname.getBytes()).toString();
+	}
+	
+	public List<String> getHandlerClassnames() {
+		return handlerClassnames;
+	}
+	
+	public void setHandlerClassnames(List<String> handlerClassnames) {
+		this.handlerClassnames = handlerClassnames;
+	}
+}
