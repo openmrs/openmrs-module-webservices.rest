043a3f5857ff2d8969106777645ecc5ca1560424
Lluis Martinez Ferrando <lluismf@gmail.com>
2015-11-04 22:18:55 +0100

    RESTWS-536: refactoring of getAuditInfo

--- omod-common/src/main/java/org/openmrs/module/webservices/rest/web/resource/impl/DelegatingSubResource.java
+++ omod-common/src/main/java/org/openmrs/module/webservices/rest/web/resource/impl/DelegatingSubResource.java
@@ -13,11 +13,11 @@
  */
 package org.openmrs.module.webservices.rest.web.resource.impl;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.Auditable;
-import org.openmrs.BaseOpenmrsObject;
-import org.openmrs.Voidable;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.web.ConversionUtil;
@@ -35,9 +35,6 @@ import org.openmrs.module.webservices.rest.web.response.ObjectNotFoundException;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import org.openmrs.util.OpenmrsUtil;
 import org.springframework.util.ReflectionUtils;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 /**
  * Base implementation of a sub-resource of a DelegatingCrudResource that delegates to a domain
@@ -241,25 +238,4 @@ public abstract class DelegatingSubResource<T, P, PR extends DelegatingCrudResou
 		return convertDelegateToRepresentation(delegate, description);
 	}
 	
-	/**
-	 * Gets the audit information of a resource.
-	 * 
-	 * @param resource the resource.
-	 * @return a {@link SimpleObject} with the audit information.
-	 * @throws Exception
-	 */
-	public SimpleObject getAuditInfo(BaseOpenmrsObject resource) throws Exception {
-		SimpleObject ret = new SimpleObject();
-		ret.put("creator", ConversionUtil.getPropertyWithRepresentation(resource, "creator", Representation.REF));
-		ret.put("dateCreated",
-		    ConversionUtil.convertToRepresentation(((Auditable) resource).getDateCreated(), Representation.DEFAULT));
-		if (resource instanceof Voidable && ((Voidable) resource).isVoided()) {
-			ret.put("voidedBy", ConversionUtil.getPropertyWithRepresentation(resource, "voidedBy", Representation.REF));
-			ret.put("dateVoided",
-			    ConversionUtil.convertToRepresentation(((Voidable) resource).getDateVoided(), Representation.DEFAULT));
-			ret.put("voidReason",
-			    ConversionUtil.convertToRepresentation(((Voidable) resource).getVoidReason(), Representation.DEFAULT));
-		}
-		return ret;
-	}
 }
--- omod-common/src/main/java/org/openmrs/module/webservices/rest/web/resource/impl/MetadataDelegatingCrudResource.java
+++ omod-common/src/main/java/org/openmrs/module/webservices/rest/web/resource/impl/MetadataDelegatingCrudResource.java
@@ -19,7 +19,6 @@ import org.apache.commons.lang.StringUtils;
 import org.openmrs.OpenmrsMetadata;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.SimpleObject;
-import org.openmrs.module.webservices.rest.web.ConversionUtil;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.PropertyGetter;
@@ -27,7 +26,6 @@ import org.openmrs.module.webservices.rest.web.annotation.RepHandler;
 import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.RefRepresentation;
-import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.response.ConversionException;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 
@@ -74,21 +72,6 @@ public abstract class MetadataDelegatingCrudResource<T extends OpenmrsMetadata> 
 		rep.addProperty("auditInfo", findMethod("getAuditInfo"));
 		rep.addSelfLink();
 		return convertDelegateToRepresentation(delegate, rep);
-	}
-	
-	public SimpleObject getAuditInfo(T delegate) throws Exception {
-		SimpleObject ret = new SimpleObject();
-		ret.put("creator", ConversionUtil.getPropertyWithRepresentation(delegate, "creator", Representation.REF));
-		ret.put("dateCreated", ConversionUtil.convertToRepresentation(delegate.getDateCreated(), Representation.DEFAULT));
-		if (delegate.isRetired()) {
-			ret.put("retiredBy", ConversionUtil.getPropertyWithRepresentation(delegate, "retiredBy", Representation.REF));
-			ret.put("dateRetired", ConversionUtil.convertToRepresentation(delegate.getDateRetired(), Representation.DEFAULT));
-			ret.put("retireReason",
-			    ConversionUtil.convertToRepresentation(delegate.getRetireReason(), Representation.DEFAULT));
-		}
-		ret.put("changedBy", ConversionUtil.getPropertyWithRepresentation(delegate, "changedBy", Representation.REF));
-		ret.put("dateChanged", ConversionUtil.convertToRepresentation(delegate.getDateChanged(), Representation.DEFAULT));
-		return ret;
 	}
 	
 	/**
