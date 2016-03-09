2d391e838e72b506a5a6fa7cd1f70bda28a22d98
Pascal Brandt <psbrandt@gmail.com>
2016-03-09 14:42:19 -0800

    Improved test case

--- omod-common/src/test/java/org/openmrs/module/webservices/rest/doc/SwaggerSpecificationCreatorTest.java
+++ omod-common/src/test/java/org/openmrs/module/webservices/rest/doc/SwaggerSpecificationCreatorTest.java
@@ -3,16 +3,15 @@
  * Version 1.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://license.openmrs.org
- *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
  * License for the specific language governing rights and limitations
  * under the License.
- *
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
 package org.openmrs.module.webservices.rest.doc;
 
+import org.dbunit.database.DatabaseConnection;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,7 +22,9 @@ import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.api.RestService;
 import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,45 +37,20 @@ public class SwaggerSpecificationCreatorTest extends BaseModuleWebContextSensiti
 		
 		Connection con = this.getConnection();
 		DatabaseMetaData metaData = con.getMetaData();
+		DatabaseConnection dbcon = new DatabaseConnection(con);
 		
 		ResultSet rs = metaData.getTables(null, "PUBLIC", "%", null);
 		while (rs.next()) {
 			String tableName = rs.getString(3);
 			
-			Statement stmt = con.createStatement();
-			
-			ResultSet resultSet = stmt.executeQuery("SELECT count(*) AS rowcount FROM " + tableName);
-			
-			resultSet.next();
-			
-			ret.put(tableName, resultSet.getInt("rowcount"));
-			
-			resultSet.close();
+			ret.put(tableName, dbcon.getRowCount(tableName));
 		}
 		
 		return ret;
 	}
 	
-	private void showTableContents(String tableName) throws Exception {
-		Connection con = this.getConnection();
-		Statement stmt = con.createStatement();
-		ResultSet resultSet = stmt.executeQuery("SELECT * FROM " + tableName);
-		
-		ResultSetMetaData metaData = resultSet.getMetaData();
-		int columnCount = metaData.getColumnCount();
-		
-		while (resultSet.next()) {
-			for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-				Object object = resultSet.getObject(columnIndex);
-				System.out.printf("%s, ", object == null ? "NULL" : object.toString());
-			}
-			System.out.printf("%n");
-		}
-	}
-	
 	@Before
 	public void init() throws Exception {
-		
 		// init REST
 		Context.getService(RestService.class).initialize();
 		
@@ -89,27 +65,21 @@ public class SwaggerSpecificationCreatorTest extends BaseModuleWebContextSensiti
 	
 	@Test
 	public void checkNoDatabaseChanges() throws Exception {
-		
 		SwaggerSpecificationCreator ssc = new SwaggerSpecificationCreator("/v1/");
 		ssc.BuildJSON();
 		
 		Map<String, Integer> afterCounts = getRowCounts();
 		
-		Assert.assertTrue(ensureCountsEqual(beforeCounts, afterCounts));
+		Assert.assertEquals("Ensure no tables are created or destroyed", beforeCounts.size(), afterCounts.size());
+		Assert.assertTrue("Ensure that no data was added or removed from any tables",
+		    ensureCountsEqual(beforeCounts, afterCounts));
 	}
 	
 	private boolean ensureCountsEqual(Map<String, Integer> beforeCounts, Map<String, Integer> afterCounts) throws Exception {
-		if (beforeCounts.size() != afterCounts.size()) {
-			System.err.println("There are a different number of tables.");
-			return false;
-		}
-		
 		for (String key : beforeCounts.keySet()) {
 			if (beforeCounts.get(key) != afterCounts.get(key)) {
 				System.err.println("The " + key + " table has a different number of rows (" + beforeCounts.get(key)
 				        + " before, " + afterCounts.get(key) + " after).");
-				
-				showTableContents(key);
 				
 				return false;
 			}
