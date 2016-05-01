358306f2958d28b15be2f6f864a700447be5484f
McArthur <amcarth2@sp16-cs498dm-03.studentspace.cs.illinois.edu>
2016-05-01 16:52:55 -0500

    removing web folder

---
+++ omod/src/test/java/org/openmrs/module/webservices/rest/web/controller/SettingsFormControllerTest.java
@@ -0,0 +1,18 @@
+package org.openmrs.module.webservices.rest.web.controller;
+
+import org.openmrs.api.APIException;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+//import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class SettingsFormControllerTest {
+	
+}
