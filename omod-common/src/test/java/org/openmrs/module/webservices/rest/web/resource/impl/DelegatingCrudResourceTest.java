1cb3fdfd49a56be95aee2447836a9e8cacb8a68c
Alex <amcarth2@illinois.edu>
2016-05-05 13:51:27 -0500

    authorization filter tests

---
+++ omod-common/src/test/java/org/openmrs/module/webservices/rest/web/resource/impl/DelegatingCrudResourceTest.java
@@ -0,0 +1,45 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.webservices.rest.web.resource.impl;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.ConversionUtil;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.api.CrudResource;
+import org.openmrs.module.webservices.rest.web.resource.api.Listable;
+import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
+import org.openmrs.module.webservices.rest.web.resource.api.Searchable;
+import org.openmrs.module.webservices.rest.web.response.IllegalPropertyException;
+import org.openmrs.module.webservices.rest.web.response.ObjectNotFoundException;
+import org.openmrs.module.webservices.rest.web.response.ResourceDoesNotSupportOperationException;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceController;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.MainSubResourceController;
+import org.openmrs.module.webservices.validation.ValidateUtil;
+
+public class DelegatingCrudResourceTest {
+
+
+}
---
+++ omod-common/src/test/java/org/openmrs/module/webservices/rest/web/resource/impl/ServiceSearcherTest.java
@@ -0,0 +1,48 @@
+package org.openmrs.module.webservices.rest.web.resource.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.LinkedList;
+import org.openmrs.api.OpenmrsService;
+import org.openmrs.api.PatientService;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.context.ServiceContext;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.resource.api.Searchable;
+import java.lang.reflect.ParameterizedType;
+
+import org.openmrs.api.AdministrationService;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import org.mockito.Matchers;
+
+public class ServiceSearcherTest {
+	
+	//@TODO rename method, clean formatting, add actual assert
+	@Test
+	public void ServiceSearcherShouldThrowExceptionOnNullArgument() {
+		ServiceContext scon = ServiceContext.getInstance();
+		PatientService pat = mock(PatientService.class);
+		AdministrationService adm = mock(AdministrationService.class);
+		when(pat.getPatients(anyString(), anyInt(), anyInt())).thenReturn(new LinkedList());
+		scon.setPatientService(pat);
+		scon.setAdministrationService(adm);
+		RequestContext rcon = new RequestContext();
+		//Context.getAdministrationService();
+		rcon.setStartIndex(0);
+		rcon.setLimit(1);
+		ServiceSearcher<String> S = new ServiceSearcher(PatientService.class, "getPatients", "getCountOfPatients");
+		
+		AlreadyPaged res = S.search("foo", rcon);
+		Assert.assertFalse(res.hasMoreResults());
+	}
+}
