fe3f1f2a4a656efe3b0ca56f0a5555973eb191a4
Alex <amcarth2@illinois.edu>
2016-05-06 22:46:44 -0500

    removing unnecessary files

--- omod/src/test/java/org/openmrs/Allergen.java
+++
@@ -1,8 +0,0 @@
-package org.openmrs;
-
-//TODO this dummy class is just for the sake of making SwaggerSpecificationCreatorTest pass.
-//it fails because the platform dependency version currently used, does not have this class.
-//after this has been solved, this dummy class can be deleted.
-public class Allergen {
-	
-}
