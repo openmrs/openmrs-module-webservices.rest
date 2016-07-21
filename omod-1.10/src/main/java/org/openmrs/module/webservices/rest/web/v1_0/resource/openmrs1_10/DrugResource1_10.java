e6fed9f74b06a7d177b3e2327d48e1703c06cf04
dkayiwa <kayiwadaniel@gmail.com>
2016-07-21 19:58:25 +0300

    Fixing: Two resources with the same name (v1/drug) must not have the same order: https://ci.openmrs.org/browse/RESTWS-RESTWS-606/log

--- omod-1.10/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_10/DrugResource1_10.java
+++ omod-1.10/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs1_10/DrugResource1_10.java
@@ -29,7 +29,7 @@ import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.DrugReso
  * {@link org.openmrs.module.webservices.rest.web.annotation.Resource} for {@link org.openmrs.Drug},
  * supporting standard CRUD operations
  */
-@Resource(name = RestConstants.VERSION_1 + "/drug", supportedClass = Drug.class, supportedOpenmrsVersions = {"1.10.*"})
+@Resource(name = RestConstants.VERSION_1 + "/drug", order = 3, supportedClass = Drug.class, supportedOpenmrsVersions = {"1.10.*"})
 public class DrugResource1_10 extends DrugResource1_8 {
 	
 	/**
