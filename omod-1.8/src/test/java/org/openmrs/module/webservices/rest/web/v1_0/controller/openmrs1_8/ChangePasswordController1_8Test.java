4ecd02f70872ed66294a6ba9e212c5d77d50fd30
Darius Jazayeri <jazayeri@alum.mit.edu>
2017-02-01 16:07:59 -0800

    formatting

--- omod-1.8/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/ChangePasswordController1_8Test.java
+++ omod-1.8/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/ChangePasswordController1_8Test.java
@@ -31,133 +31,132 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 public class ChangePasswordController1_8Test extends RestControllerTestUtils {
-    
-    private static final String PASSWORD_URI = "password";
-    
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
-    
-    @Autowired
-    @Qualifier("userService")
-    private UserService service;
-    
-    
-    @Test
-    public void updateUser_shouldUpdateTheUserPassword() throws Exception {
-        User user = service.getUserByUuid(RestTestConstants1_8.USER_UUID);
-        assertNotNull(user);
-        assertNotEquals(user, Context.getAuthenticatedUser());
-        final String username = user.getUsername();
-        final String newPassword = "SomeOtherPassword123";
-        
-        ContextAuthenticationException exception = null;
-        try {
-            Context.authenticate(username, newPassword);
-        }
-        catch (ContextAuthenticationException e) {
-            exception = e;
-        }
-        assertNotNull(exception);
-        assertEquals("Invalid username and/or password: " + username, exception.getMessage());
-        
-        handle(newPostRequest("password" + "/" + user.getUuid(), "{\"newPassword\":\"" + newPassword + "\"}"));
-        Context.logout();
-        
-        Context.authenticate(username, newPassword);
-        assertEquals(user, Context.getAuthenticatedUser());
-    }
-    
-    @Test
-    public void testChangeUsersOwnPassword() throws Exception {
-        setUpUser("butch");
-        
-        String oldPassword = "SomeOtherPassword123";
-        String newPassword = "newPassword9";
-        
-        MockHttpServletResponse response = handle(newPostRequest(PASSWORD_URI, "{\"newPassword\":\"" + newPassword + "\""
-                + "," + "\"oldPassword\":\"" + oldPassword + "\"}"));
-        assertEquals(200, response.getStatus());
-    }
-    
-    @Test
-    public void testChangeUsersOwnPasswordWithOutAuthentication() throws Exception {
-        // we log out, so there is no authenticated user
-        Context.logout();
-        String oldPassword = "SomeOtherPassword123";
-        String newPassword = "newPassword9";
-        
-        expectedException.expect(APIAuthenticationException.class);
-        expectedException.expectMessage("Must be authenticated to change your own password");
-        
-        handle(newPostRequest(PASSWORD_URI, "{\"newPassword\":\"" + newPassword + "\"" + "," + "\"oldPassword\":\""
-                + oldPassword + "\"}"));
-    }
-    
-    @Test
-    public void testChangeUsersOwnPasswordWithIncorrectOldPassword() throws Exception {
-        setUpUser("butch");
-        
-        String wrongOldPassword = "WrongPassword";
-        String newPassword = "newPassword9";
-        
-        expectedException.expect(ValidationException.class);
-        expectedException.expectMessage("Passwords don't match");
-        
-        handle(newPostRequest(PASSWORD_URI, "{\"newPassword\":\"" + newPassword + "\"" + "," + "\"oldPassword\":\""
-                + wrongOldPassword + "\"}"));
-    }
-    
-    @Test
-    public void testUserChangeOtherUsersPassword() throws Exception {
-        User authenticatedUser = setUpUser("daemon");
-        
-        Role role = new Role("Privileged Role");
-        role.addPrivilege(new Privilege(PrivilegeConstants.EDIT_USER_PASSWORDS));
-        authenticatedUser.addRole(role);
-        
-        String newPassword = "newPassword9";
-        
-        MockHttpServletResponse response = handle(newPostRequest(PASSWORD_URI + "/" + RestTestConstants1_8.USER_UUID,
-                "{\"password\":\"" + newPassword + "\"}"));
-        
-        assertEquals(200, response.getStatus());
-    }
-    
-    @Test
-    public void testUserChangeOtherUsersPasswordWithOutPrivilege() throws Exception {
-        setUpUser("daemon");
-        
-        String newPassword = "newPassword9";
-        
-        expectedException.expect(APIAuthenticationException.class);
-        expectedException.expectMessage("Privileges required: [Edit User Passwords]");
-        
-        handle(newPostRequest(PASSWORD_URI + "/" + RestTestConstants1_8.USER_UUID, "{\"newPassword\":\"" + newPassword
-                + "\"}"));
-        
-    }
-    
-    @Test
-    public void testThrowExceptionIfUserIsNotAvailable() throws Exception {
-        setUpUser("daemon");
-        
-        String newPassword = "newPassword9";
-        
-        expectedException.expect(NullPointerException.class);
-        
-        handle(newPostRequest(PASSWORD_URI + "/" + "someRandomUserUuid", "{\"newPassword\":\"" + newPassword + "\"}"));
-    }
-    
-    private User setUpUser(String userName) throws Exception {
-        User user = service.getUserByUsername(userName);
-        final String newPassword = "SomeOtherPassword123";
-        
-        service.changePassword(user, newPassword);
-        
-        Context.logout();
-        
-        Context.authenticate(userName, newPassword);
-        return Context.getAuthenticatedUser();
-    }
-    
+	
+	private static final String PASSWORD_URI = "password";
+	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
+	@Autowired
+	@Qualifier("userService")
+	private UserService service;
+	
+	@Test
+	public void updateUser_shouldUpdateTheUserPassword() throws Exception {
+		User user = service.getUserByUuid(RestTestConstants1_8.USER_UUID);
+		assertNotNull(user);
+		assertNotEquals(user, Context.getAuthenticatedUser());
+		final String username = user.getUsername();
+		final String newPassword = "SomeOtherPassword123";
+		
+		ContextAuthenticationException exception = null;
+		try {
+			Context.authenticate(username, newPassword);
+		}
+		catch (ContextAuthenticationException e) {
+			exception = e;
+		}
+		assertNotNull(exception);
+		assertEquals("Invalid username and/or password: " + username, exception.getMessage());
+		
+		handle(newPostRequest("password" + "/" + user.getUuid(), "{\"newPassword\":\"" + newPassword + "\"}"));
+		Context.logout();
+		
+		Context.authenticate(username, newPassword);
+		assertEquals(user, Context.getAuthenticatedUser());
+	}
+	
+	@Test
+	public void testChangeUsersOwnPassword() throws Exception {
+		setUpUser("butch");
+		
+		String oldPassword = "SomeOtherPassword123";
+		String newPassword = "newPassword9";
+		
+		MockHttpServletResponse response = handle(newPostRequest(PASSWORD_URI, "{\"newPassword\":\"" + newPassword + "\""
+		        + "," + "\"oldPassword\":\"" + oldPassword + "\"}"));
+		assertEquals(200, response.getStatus());
+	}
+	
+	@Test
+	public void testChangeUsersOwnPasswordWithOutAuthentication() throws Exception {
+		// we log out, so there is no authenticated user
+		Context.logout();
+		String oldPassword = "SomeOtherPassword123";
+		String newPassword = "newPassword9";
+		
+		expectedException.expect(APIAuthenticationException.class);
+		expectedException.expectMessage("Must be authenticated to change your own password");
+		
+		handle(newPostRequest(PASSWORD_URI, "{\"newPassword\":\"" + newPassword + "\"" + "," + "\"oldPassword\":\""
+		        + oldPassword + "\"}"));
+	}
+	
+	@Test
+	public void testChangeUsersOwnPasswordWithIncorrectOldPassword() throws Exception {
+		setUpUser("butch");
+		
+		String wrongOldPassword = "WrongPassword";
+		String newPassword = "newPassword9";
+		
+		expectedException.expect(ValidationException.class);
+		expectedException.expectMessage("Passwords don't match");
+		
+		handle(newPostRequest(PASSWORD_URI, "{\"newPassword\":\"" + newPassword + "\"" + "," + "\"oldPassword\":\""
+		        + wrongOldPassword + "\"}"));
+	}
+	
+	@Test
+	public void testUserChangeOtherUsersPassword() throws Exception {
+		User authenticatedUser = setUpUser("daemon");
+		
+		Role role = new Role("Privileged Role");
+		role.addPrivilege(new Privilege(PrivilegeConstants.EDIT_USER_PASSWORDS));
+		authenticatedUser.addRole(role);
+		
+		String newPassword = "newPassword9";
+		
+		MockHttpServletResponse response = handle(newPostRequest(PASSWORD_URI + "/" + RestTestConstants1_8.USER_UUID,
+		    "{\"password\":\"" + newPassword + "\"}"));
+		
+		assertEquals(200, response.getStatus());
+	}
+	
+	@Test
+	public void testUserChangeOtherUsersPasswordWithOutPrivilege() throws Exception {
+		setUpUser("daemon");
+		
+		String newPassword = "newPassword9";
+		
+		expectedException.expect(APIAuthenticationException.class);
+		expectedException.expectMessage("Privileges required: [Edit User Passwords]");
+		
+		handle(newPostRequest(PASSWORD_URI + "/" + RestTestConstants1_8.USER_UUID, "{\"newPassword\":\"" + newPassword
+		        + "\"}"));
+		
+	}
+	
+	@Test
+	public void testThrowExceptionIfUserIsNotAvailable() throws Exception {
+		setUpUser("daemon");
+		
+		String newPassword = "newPassword9";
+		
+		expectedException.expect(NullPointerException.class);
+		
+		handle(newPostRequest(PASSWORD_URI + "/" + "someRandomUserUuid", "{\"newPassword\":\"" + newPassword + "\"}"));
+	}
+	
+	private User setUpUser(String userName) throws Exception {
+		User user = service.getUserByUsername(userName);
+		final String newPassword = "SomeOtherPassword123";
+		
+		service.changePassword(user, newPassword);
+		
+		Context.logout();
+		
+		Context.authenticate(userName, newPassword);
+		return Context.getAuthenticatedUser();
+	}
+	
 }
