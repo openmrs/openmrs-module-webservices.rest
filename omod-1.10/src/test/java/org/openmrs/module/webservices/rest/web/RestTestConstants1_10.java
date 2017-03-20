f341eeb1efe3a234bab3f9f900aa366df81a310b
vtuwei <vtuwei@ampath.or.ke>
2017-03-20 15:13:07 +0300

    RESTWS-647: Searching for Orderable Concepts

--- omod-1.10/src/test/java/org/openmrs/module/webservices/rest/web/RestTestConstants1_10.java
+++ omod-1.10/src/test/java/org/openmrs/module/webservices/rest/web/RestTestConstants1_10.java
@@ -18,4 +18,6 @@ public class RestTestConstants1_10 {
 	public final static String ORDER_UUID = "2c96f25c-4949-4f72-9931-d808fbc226df";
 	
 	public final static String ORDER_TYPE_UUID = "52a447d3-a64a-11e3-9aeb-50e549534c5e";
+	
+	public final static String COUGH_SYRUP_UUID = "0cbe2ed3-cd5f-4f46-9459-26127c9265ab";
 }
