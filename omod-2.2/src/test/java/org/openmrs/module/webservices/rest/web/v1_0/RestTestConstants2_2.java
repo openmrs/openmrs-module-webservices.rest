6fcc43c571d76e12b124995783052d30327a887c
Brian Njenga <bkuriakb@gmail.com>
2018-02-20 14:50:46 +0300

    RESTWS-698 Add bahmni program enrollment changes (#323)
    Remove unwanted files
    
    Add constants for program enrollment
    
    Add missing license header to xml files
    
    Add omod 2.2 to list of loaded modules
    
    Add openmrs proper version
    
    Remove unused import
    
    Change method for getting service context
    
    Rename files to match 2_2 criteria
    
    Add omod-2.2 rest constants
    
    Revert changes
    
    Change constant variable reference
    
    Remove version 1.12
    
    Restore commented out lines
    
    Add the proper rest test constants import in program attribute resource test
    
    Remove context configurations
    
    RESTWS-698 Remove unused dependencies
    
    RESTWS-698 Remove unused import and commented out code
    
    RESTWS-698 Revert reset constants changes

---
+++ omod-2.2/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/RestTestConstants2_2.java
@@ -0,0 +1,19 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.webservices.rest.web.v1_0;
+
+public class RestTestConstants2_2 {
+	
+	public final static String PATIENT_PROGRAM_ATTRIBUTE_UUID = "3a2bdb18-6faa-11e0-8414-001e378eb67e";
+	
+	public final static String PROGRAM_ATTRIBUTE_TYPE_UUID = "d7477c21-bfc3-4922-9591-e89d8b9c8efb";
+	
+	public final static String PATIENT_PROGRAM_UUID = "9119b9f8-af3d-4ad8-9e2e-2317c3de91c6";
+}
