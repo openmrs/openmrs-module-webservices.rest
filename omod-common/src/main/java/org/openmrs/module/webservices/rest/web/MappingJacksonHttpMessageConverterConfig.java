cf589fd0d1dad175ca7bef6a396b4a97a37b5bb0
wluyima <wyclif@openmrs.org>
2018-08-28 18:00:20 -0400

    TRUNK-5193 - Follow up to address review comments

--- omod-common/src/main/java/org/openmrs/module/webservices/rest/web/MappingJacksonHttpMessageConverterConfig.java
+++ omod-common/src/main/java/org/openmrs/module/webservices/rest/web/MappingJacksonHttpMessageConverterConfig.java
@@ -14,24 +14,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 
+/**
+ * In spring 3.1.2 MappingJacksonHttpMessageConverter was replaced with
+ * MappingJackson2HttpMessageConverter and eventually removed in version 4.1.0. This bean
+ * configuration class allows the module to run on both pre and post 4.1.0 versions of Spring by
+ * loading the new class if it's available in the spring version that OpenMRS is running on
+ * otherwise it falls back to the old one.
+ */
 @Configuration
-public class MappingJacksonHttpMessageConverter {
+public class MappingJacksonHttpMessageConverterConfig {
 	
 	private static final String PACKAGE = "org.springframework.http.converter.json.";
 	
-	private static final String OLD_CLASS_NAME = PACKAGE + "MappingJacksonHttpMessageConverter";
+	private static final String CLASS_BEFORE_SPRING_4_1 = PACKAGE + "MappingJacksonHttpMessageConverter";
 	
-	private static final String NEW_CLASS_NAME = PACKAGE + "MappingJackson2HttpMessageConverter";
+	private static final String CLASS_FROM_SPRING_4_1 = PACKAGE + "MappingJackson2HttpMessageConverter";
 	
 	@Bean(name = "jsonHttpMessageConverter")
 	public HttpMessageConverter getMappingJacksonHttpMessageConverter() throws Exception {
 		
 		Class<?> clazz;
 		try {
-			clazz = Context.loadClass(NEW_CLASS_NAME);
+			clazz = Context.loadClass(CLASS_BEFORE_SPRING_4_1);
 		}
 		catch (ClassNotFoundException e) {
-			clazz = Context.loadClass(OLD_CLASS_NAME);
+			clazz = Context.loadClass(CLASS_FROM_SPRING_4_1);
 		}
 		
 		return (HttpMessageConverter) clazz.newInstance();
