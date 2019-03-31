021d468e75608f927109ab5d0992f5a1c1081c38
mozzy11 <mozzymutesa@gmail.com>
2019-03-31 16:24:14 +0300

    RSTWS-680:Added OrderGroupResource and added Tests for my changes

--- omod-1.12/src/test/java/org/openmrs/module/webservices/rest/web/RestTestConstants1_12.java
+++ omod-1.12/src/test/java/org/openmrs/module/webservices/rest/web/RestTestConstants1_12.java
@@ -18,4 +18,25 @@ public class RestTestConstants1_12 {
 	public final static String ORDER_SET_MEMBER_UUID = "order_set_member_uuid1";
 	
 	public static final String DRUG_UUID = "05ec820a-d297-44e3-be6e-698531d9dd3f";
+	
+	public final static String ORDER_GROUP_TEST_DATA_SET = "orderGroupTestDataset.xml";
+	
+	public final static String ORDER_GROUP_UUID = "1c96f25c-4949-4f72-9931-d808fbcdb613";
+	
+	public final static String ORDER_GROUP_DISPLAY = "1234 - Mr. Horatio Peeter Hornblower2 - Order_Set_1";
+	
+	public final static String ORDER_GROUP_ENCOUNTER_UUID = "33d70956-b359-452a-b3da-b69c8ab459ce";
+	
+	public final static String ORDER_GROUP_PATIENT_UUID = "19887889-7110-46a7-a9f0-ebf1eb4bbd48";
+	
+	public final static String ORDER_GROUP_ORDERSET_UUID = "2d3fb1d0-ae06-22e3-a5e2-0140211c1001";
+	
+	public final static String ORDER_GROUP_ORDER_TYPE_UUID = "2d3fb1d0-ae06-22e3-a5e2-0140211c9a66";
+	
+	public final static String ORDER_GROUP_CONCEPT_UUID = "985ff1a2-c2ef-49fd-836f-8a1d936d9ef9";
+	
+	public final static String ORDER_GROUP_CARESETTING_UUID = "6f0c9a92-6f24-11e3-af88-005056821db0";
+	
+	public final static String ORDER_GROUP_ODERER_UUID = "c2299800-cca9-11e0-9572-0800200c9a66";
+	
 }
