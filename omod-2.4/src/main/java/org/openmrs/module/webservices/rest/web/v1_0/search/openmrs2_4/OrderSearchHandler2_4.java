8a602eee4b2cf027e7f915b88fe88644c409080f
Cosmin Ioan <cioan@pih.org>
2019-11-08 11:29:23 -0500

    RESTWS-757, expand order search handler

--- omod-2.4/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs2_4/OrderSearchHandler2_4.java
+++ omod-2.4/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs2_4/OrderSearchHandler2_4.java
@@ -7,7 +7,7 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
-package org.openmrs.module.webservices.rest.web.v1_0.search.openmrs2_2;
+package org.openmrs.module.webservices.rest.web.v1_0.search.openmrs2_4;
 
 import org.apache.commons.lang.StringUtils;
 import org.openmrs.*;
@@ -32,12 +32,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.*;
 
-/**
- * Returns orders based on search by patient, care setting, concepts, order types, activated date
- * range, and voided orders
- */
 @Component
-public class OrderSearchHandler2_2 implements SearchHandler {
+public class OrderSearchHandler2_4 implements SearchHandler {
 	
 	public static final String REQUEST_PARAM_PATIENT = "patient";
 	
@@ -50,6 +46,18 @@ public class OrderSearchHandler2_2 implements SearchHandler {
 	public static final String REQUEST_PARAM_ACTIVATED_ON_OR_BEFORE_DATE = "activatedOnOrBeforeDate";
 	
 	public static final String REQUEST_PARAM_ACTIVATED_ON_OR_AFTER_DATE = "activatedOnOrAfterDate";
+	
+	public static final String REQUEST_PARAM_DATE_STOPPED_ON_OR_BEFORE_DATE = "dateStoppedOnOrBeforeDate";
+	
+	public static final String REQUEST_PARAM_AUTO_EXPIRE_ON_OR_BEFORE_DATE = "autoExpireOnOrBeforeDate";
+	
+	public static final String REQUEST_PARAM_CANCELED_OR_AUTO_EXPIRE_ON_OR_BEFORE_DATE = "canceledOrExpiredOnOrBeforeDate";
+	
+	public static final String REQUEST_PARAM_ACTION = "action";
+	
+	public static final String REQUEST_PARAM_FULFILLER_STATUS = "fulfillerStatus";
+	
+	public static final String REQUEST_PARAM_EXCLUDE_CANCELED_AND_EXPIRED = "excludeCanceledAndExpired";
 	
 	public static final String REQUEST_PARAM_INCLUDE_VOIDED = "includeVoided";
 	
@@ -74,10 +82,16 @@ public class OrderSearchHandler2_2 implements SearchHandler {
 	            REQUEST_PARAM_ORDER_TYPES,
 	            REQUEST_PARAM_ACTIVATED_ON_OR_BEFORE_DATE,
 	            REQUEST_PARAM_ACTIVATED_ON_OR_AFTER_DATE,
+	            REQUEST_PARAM_DATE_STOPPED_ON_OR_BEFORE_DATE,
+	            REQUEST_PARAM_AUTO_EXPIRE_ON_OR_BEFORE_DATE,
+	            REQUEST_PARAM_CANCELED_OR_AUTO_EXPIRE_ON_OR_BEFORE_DATE,
+	            REQUEST_PARAM_ACTION,
+	            REQUEST_PARAM_FULFILLER_STATUS,
+	            REQUEST_PARAM_EXCLUDE_CANCELED_AND_EXPIRED,
 	            REQUEST_PARAM_INCLUDE_VOIDED).build();
 	
 	private final SearchConfig searchConfig = new SearchConfig("default", RestConstants.VERSION_1
-	        + "/order", Arrays.asList("2.2.*", "2.3.*"), searchQuery);
+	        + "/order", Arrays.asList("2.4.*"), searchQuery);
 	
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.resource.api.SearchHandler#getSearchConfig()
@@ -88,7 +102,7 @@ public class OrderSearchHandler2_2 implements SearchHandler {
 	}
 	
 	/**
-	 * @see org.openmrs.module.webservices.rest.web.resource.api.SearchHandler#search(org.openmrs.module.webservices.rest.web.RequestContext)
+	 * @see SearchHandler#search(RequestContext)
 	 */
 	@Override
 	public PageableResult search(RequestContext context) throws ResponseException {
@@ -99,14 +113,35 @@ public class OrderSearchHandler2_2 implements SearchHandler {
 		String orderTypeUuids = context.getParameter("orderTypes");
 		String activatedOnOrBeforeDateStr = context.getParameter("activatedOnOrBeforeDate");
 		String activatedOnOrAfterDateStr = context.getParameter("activatedOnOrAfterDate");
+		String dateStoppedOnOrBeforeDateStr = context.getParameter(REQUEST_PARAM_DATE_STOPPED_ON_OR_BEFORE_DATE);
+		String autoExpireOnOrBeforeDateStr = context.getParameter(REQUEST_PARAM_AUTO_EXPIRE_ON_OR_BEFORE_DATE);
+		String canceledOrExpiredOnOrBeforeDateStr = context
+		        .getParameter(REQUEST_PARAM_CANCELED_OR_AUTO_EXPIRE_ON_OR_BEFORE_DATE);
+		String actionStr = context.getParameter(REQUEST_PARAM_ACTION);
+		String fulfillerStatusStr = context.getParameter(REQUEST_PARAM_FULFILLER_STATUS);
+		String excludeCanceledAndExpiredStr = context.getParameter(REQUEST_PARAM_EXCLUDE_CANCELED_AND_EXPIRED);
 		String includeVoidedStr = context.getParameter("includeVoided");
 		
 		// build search criteria for order service
 		boolean includeVoided = StringUtils.isNotBlank(includeVoidedStr) ? Boolean.parseBoolean(includeVoidedStr) : false;
+		// by default the Canceled(dateStopped != null) and Expired(autoExpire < today) orders are excluded
+		boolean excludeCanceledAndExpired = StringUtils.isNotBlank(excludeCanceledAndExpiredStr) ? Boolean
+		        .parseBoolean(excludeCanceledAndExpiredStr) : false;
+		
 		Date activatedOnOrBeforeDate = StringUtils.isNotBlank(activatedOnOrBeforeDateStr) ?
 		        (Date) ConversionUtil.convert(activatedOnOrBeforeDateStr, Date.class) : null;
 		Date activatedOnOrAfterDate = StringUtils.isNotBlank(activatedOnOrAfterDateStr) ?
 		        (Date) ConversionUtil.convert(activatedOnOrAfterDateStr, Date.class) : null;
+		Date dateStoppedOnOrBeforeDate = StringUtils.isNotBlank(dateStoppedOnOrBeforeDateStr) ?
+		        (Date) ConversionUtil.convert(dateStoppedOnOrBeforeDateStr, Date.class) : null;
+		Date autoExpireOnOrBeforeDate = StringUtils.isNotBlank(autoExpireOnOrBeforeDateStr) ?
+		        (Date) ConversionUtil.convert(autoExpireOnOrBeforeDateStr, Date.class) : null;
+		Date canceledOrExpiredOnOrBeforeDate = StringUtils.isNotBlank(canceledOrExpiredOnOrBeforeDateStr) ?
+		        (Date) ConversionUtil.convert(canceledOrExpiredOnOrBeforeDateStr, Date.class) : null;
+		Order.Action action = StringUtils.isNotBlank(actionStr) ? Order.Action
+		        .valueOf(actionStr) : null;
+		Order.FulfillerStatus fulfillerStatus = StringUtils.isNotBlank(fulfillerStatusStr) ? Order.FulfillerStatus
+		        .valueOf(fulfillerStatusStr) : null;
 		List<Concept> concepts = null;
 		List<OrderType> orderTypes = null;
 		
@@ -168,6 +203,12 @@ public class OrderSearchHandler2_2 implements SearchHandler {
 		        .setOrderTypes(orderTypes)
 		        .setActivatedOnOrBeforeDate(activatedOnOrBeforeDate)
 		        .setActivatedOnOrAfterDate(activatedOnOrAfterDate)
+		        .setDateStoppedOnOrBeforeDate(dateStoppedOnOrBeforeDate)
+		        .setAutoExpireOnOrBeforeDate(autoExpireOnOrBeforeDate)
+		        .setCanceledOrExpiredOnOrBeforeDate(canceledOrExpiredOnOrBeforeDate)
+		        .setAction(action)
+		        .setFulfillerStatus(fulfillerStatus)
+		        .setExcludeCanceledAndExpired(excludeCanceledAndExpired)
 		        .setIncludeVoided(includeVoided)
 		        .build();
 		
---
+++ omod-2.4/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs2_4/OrderSearchHandler2_4Test.java
@@ -0,0 +1,123 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.webservices.rest.web.v1_0.search.openmrs2_4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openmrs.Order;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestTestConstants1_10;
+import org.openmrs.module.webservices.rest.web.RestTestConstants1_8;
+import org.openmrs.module.webservices.rest.web.response.ObjectNotFoundException;
+import org.openmrs.module.webservices.rest.web.v1_0.RestTestConstants2_4;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.RestControllerTestUtils;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+public class OrderSearchHandler2_4Test extends RestControllerTestUtils {
+	
+	protected String getURI() {
+		return "order";
+	}
+	
+	/**
+	 * @verifies returns orders matching autoExpireOnOrBeforeDate
+	 * @see OrderSearchHandler2_4#search(RequestContext)
+	 */
+	@Test
+	public void getSearchConfig_shouldReturnOrdersAutoExpiredBeforeDate() throws Exception {
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+		req.addParameter("s", "default");
+		req.addParameter("autoExpireOnOrBeforeDate", "2008-09-30");
+		
+		SimpleObject result = deserialize(handle(req));
+		List<Order> orders = result.get("results");
+		Assert.assertEquals(4, orders.size());
+	}
+	
+	/**
+	 * @verifies returns orders matching dateStoppedOnOrBeforeDate
+	 * @see OrderSearchHandler2_4#search(RequestContext)
+	 */
+	@Test
+	public void getSearchConfig_shouldReturnOrdersStoppedBeforeDate() throws Exception {
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+		req.addParameter("s", "default");
+		req.addParameter("dateStoppedOnOrBeforeDate", "2006-05-30");
+		
+		SimpleObject result = deserialize(handle(req));
+		List<Order> orders = result.get("results");
+		Assert.assertEquals(4, orders.size());
+	}
+	
+	/**
+	 * @verifies returns orders matching autoExpireOnOrBeforeDate
+	 * @see OrderSearchHandler2_4#search(RequestContext)
+	 */
+	@Test
+	public void getSearchConfig_shouldReturnOnlyCanceledOrAutoExpiredBeforeDate() throws Exception {
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+		req.addParameter("s", "default");
+		req.addParameter("canceledOrExpiredOnOrBeforeDate", "2008-09-30");
+		
+		SimpleObject result = deserialize(handle(req));
+		List<Order> orders = result.get("results");
+		Assert.assertEquals(7, orders.size());
+	}
+	
+	/**
+	 * @verifies returns orders matching fulfillerStatus
+	 * @see OrderSearchHandler2_4#search(RequestContext)
+	 */
+	@Test
+	public void getSearchConfig_shouldReturnOrdersWithFulfillerStatusCompleted() throws Exception {
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+		req.addParameter("s", "default");
+		req.addParameter("fulfillerStatus", "COMPLETED");
+		
+		SimpleObject result = deserialize(handle(req));
+		List<Order> orders = result.get("results");
+		Assert.assertEquals(1, orders.size());
+	}
+	
+	/**
+	 * @verifies returns orders exluding Canceled and Expired
+	 * @see OrderSearchHandler2_4#search(RequestContext)
+	 */
+	@Test
+	public void getSearchConfig_shouldNotReturnCanceledOrExpired() throws Exception {
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+		req.addParameter("s", "default");
+		req.addParameter("excludeCanceledAndExpired", "true");
+		
+		SimpleObject result = deserialize(handle(req));
+		List<Order> orders = result.get("results");
+		Assert.assertEquals(6, orders.size());
+	}
+	
+	/**
+	 * @verifies returns orders matching action
+	 * @see OrderSearchHandler2_4#search(RequestContext)
+	 */
+	@Test
+	public void getSearchConfig_shouldReturnDiscontinuedOrders() throws Exception {
+		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+		req.addParameter("s", "default");
+		req.addParameter("action", "DISCONTINUE");
+		
+		SimpleObject result = deserialize(handle(req));
+		List<Order> orders = result.get("results");
+		Assert.assertEquals(2, orders.size());
+	}
+	
+}
