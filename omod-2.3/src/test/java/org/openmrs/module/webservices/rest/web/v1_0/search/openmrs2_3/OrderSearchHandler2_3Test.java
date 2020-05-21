165f11d21ceda73f65610653a221e3a51199e702
mgoodrich <mark.goodrich@live.com>
2020-05-21 14:36:43 -0400

    RESTWS-775:Expand order search handler to support searching on Accession Number and Order Number

--- omod-2.3/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs2_3/OrderSearchHandler2_3Test.java
+++ omod-2.3/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs2_3/OrderSearchHandler2_3Test.java
@@ -22,11 +22,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
-	
+
 	protected String getURI() {
 		return "order";
 	}
-	
+
 	/**
 	 * @verifies returns orders matching autoExpireOnOrBeforeDate
 	 * @see OrderSearchHandler2_3#search(RequestContext)
@@ -36,12 +36,12 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
 		req.addParameter("s", "default");
 		req.addParameter("autoExpireOnOrBeforeDate", "2008-09-30");
-		
+
 		SimpleObject result = deserialize(handle(req));
 		List<Order> orders = result.get("results");
 		Assert.assertEquals(4, orders.size());
 	}
-	
+
 	/**
 	 * @verifies returns orders with dateStopped not null
 	 * @see OrderSearchHandler2_3#search(RequestContext)
@@ -51,12 +51,12 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
 		req.addParameter("s", "default");
 		req.addParameter("isStopped", "true");
-		
+
 		SimpleObject result = deserialize(handle(req));
 		List<Order> orders = result.get("results");
 		Assert.assertEquals(4, orders.size());
 	}
-	
+
 	/**
 	 * @verifies returns orders matching autoExpireOnOrBeforeDate
 	 * @see OrderSearchHandler2_3#search(RequestContext)
@@ -66,12 +66,12 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
 		req.addParameter("s", "default");
 		req.addParameter("canceledOrExpiredOnOrBeforeDate", "2008-09-30");
-		
+
 		SimpleObject result = deserialize(handle(req));
 		List<Order> orders = result.get("results");
 		Assert.assertEquals(7, orders.size());
 	}
-	
+
 	/**
 	 * @verifies returns orders matching fulfillerStatus
 	 * @see OrderSearchHandler2_3#search(RequestContext)
@@ -81,12 +81,12 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
 		req.addParameter("s", "default");
 		req.addParameter("fulfillerStatus", "COMPLETED");
-		
+
 		SimpleObject result = deserialize(handle(req));
 		List<Order> orders = result.get("results");
 		Assert.assertEquals(1, orders.size());
 	}
-	
+
 	/**
 	 * @verifies returns orders matching fulfillerStatus RECEIVED or null
 	 * @see OrderSearchHandler2_3#search(RequestContext)
@@ -98,7 +98,7 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 		req.addParameter("v", "custom:(id,uuid,display,orderNumber,dateActivated,fulfillerStatus)");
 		req.addParameter("fulfillerStatus", "RECEIVED");
 		req.addParameter("includeNullFulfillerStatus", "true");
-		
+
 		SimpleObject result = deserialize(handle(req));
 		List<Object> orders = (List<Object>) result.get("results");
 		Assert.assertEquals(12, orders.size());
@@ -111,7 +111,7 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 			}
 		}
 	}
-	
+
 	/**
 	 * @verifies returns orders matching fulfillerStatus not null
 	 * @see OrderSearchHandler2_3#search(RequestContext)
@@ -122,7 +122,7 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 		req.addParameter("s", "default");
 		req.addParameter("v", "custom:(id,uuid,display,orderNumber,dateActivated,fulfillerStatus)");
 		req.addParameter("includeNullFulfillerStatus", "false");
-		
+
 		SimpleObject result = deserialize(handle(req));
 		List<Object> orders = (List<Object>) result.get("results");
 		Assert.assertEquals(3, orders.size());
@@ -130,7 +130,7 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 			Assert.assertNotNull(PropertyUtils.getProperty(order, "fulfillerStatus"));
 		}
 	}
-	
+
 	/**
 	 * @verifies returns orders matching fulfillerStatus = null
 	 * @see OrderSearchHandler2_3#search(RequestContext)
@@ -141,7 +141,7 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 		req.addParameter("s", "default");
 		req.addParameter("v", "custom:(id,uuid,display,orderNumber,dateActivated,fulfillerStatus)");
 		req.addParameter("includeNullFulfillerStatus", "true");
-		
+
 		SimpleObject result = deserialize(handle(req));
 		List<Object> orders = (List<Object>) result.get("results");
 		Assert.assertEquals(10, orders.size());
@@ -149,7 +149,7 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 			Assert.assertNull(PropertyUtils.getProperty(order, "fulfillerStatus"));
 		}
 	}
-	
+
 	/**
 	 * @verifies returns orders exluding Canceled and Expired
 	 * @see OrderSearchHandler2_3#search(RequestContext)
@@ -159,12 +159,12 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
 		req.addParameter("s", "default");
 		req.addParameter("excludeCanceledAndExpired", "true");
-		
+
 		SimpleObject result = deserialize(handle(req));
 		List<Order> orders = result.get("results");
 		Assert.assertEquals(6, orders.size());
 	}
-	
+
 	/**
 	 * @verifies returns orders matching action
 	 * @see OrderSearchHandler2_3#search(RequestContext)
@@ -174,12 +174,12 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 		MockHttpServletRequest req = request(RequestMethod.GET, getURI());
 		req.addParameter("s", "default");
 		req.addParameter("action", "DISCONTINUE");
-		
+
 		SimpleObject result = deserialize(handle(req));
 		List<Order> orders = result.get("results");
 		Assert.assertEquals(2, orders.size());
 	}
-	
+
 	/**
 	 * @verifies returns orders that are not DISCONTINUE
 	 * @see OrderSearchHandler2_3#search(RequestContext)
@@ -190,7 +190,7 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 		req.addParameter("s", "default");
 		req.addParameter("v", "custom:(id,uuid,display,orderNumber,dateActivated,fulfillerStatus,action)");
 		req.addParameter("excludeDiscontinueOrders", "true");
-		
+
 		SimpleObject result = deserialize(handle(req));
 		List<Object> orders = (List<Object>) result.get("results");
 		Assert.assertEquals(11, orders.size());
@@ -198,5 +198,27 @@ public class OrderSearchHandler2_3Test extends RestControllerTestUtils {
 			Assert.assertNotEquals(PropertyUtils.getProperty(order, "action"), "DISCONTINUE");
 		}
 	}
-	
+
+	@Test
+    public void getSearchConfig_shouldReturnOrdersByOrderNumber() throws Exception {
+        MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+        req.addParameter("orderNumber", "ORD-7");
+
+        SimpleObject result = deserialize(handle(req));
+        List<Order> orders = result.get("results");
+        Assert.assertEquals(1, orders.size());
+        Assert.assertEquals(PropertyUtils.getProperty(orders.get(0), "uuid"), "2c96f25c-4949-4f72-9931-d808fbc226df");
+    }
+
+    @Test
+    public void getSearchConfig_shouldReturnOrdersByAccessionNumber() throws Exception {
+        MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+        req.addParameter("accessionNumber", "ACC-123");
+
+        SimpleObject result = deserialize(handle(req));
+        List<Order> orders = result.get("results");
+        Assert.assertEquals(1, orders.size());
+        Assert.assertEquals(PropertyUtils.getProperty(orders.get(0), "uuid"), "e1f95924-697a-11e3-bd76-0800271c1b75");
+    }
+
 }
