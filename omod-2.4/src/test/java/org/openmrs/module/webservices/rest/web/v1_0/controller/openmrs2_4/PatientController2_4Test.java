f5168bef63a816937731b2af31e11817a814c474
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2021-07-08 18:41:49 +0200

    RESTWS-842: Add ignored test

--- omod-2.4/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs2_4/PatientController2_4Test.java
+++ omod-2.4/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs2_4/PatientController2_4Test.java
@@ -9,26 +9,70 @@
  */
 package org.openmrs.module.webservices.rest.web.v1_0.controller.openmrs2_4;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.openmrs.Patient;
+import org.openmrs.PatientIdentifier;
+import org.openmrs.Person;
+import org.openmrs.PersonName;
 import org.openmrs.api.context.Context;
+import org.openmrs.api.db.PatientDAO;
+import org.openmrs.api.db.PersonDAO;
+import org.openmrs.api.db.hibernate.HibernatePatientDAO;
+import org.openmrs.api.db.hibernate.HibernatePersonDAO;
 import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.test.Util;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.RestControllerTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.sql.Date;
 
 import static org.junit.Assert.assertEquals;
 
 public class PatientController2_4Test extends RestControllerTestUtils {
 
+	@Autowired
+	private PatientDAO hibernatePatientDao;
+
+	@Autowired
+	private PersonDAO hibernatePersonDAO;
+
 	@Test
+	@Ignore("TRUNK-6010: The generated SQL contains syntax error in Core before version 2.5. This has been fixed on commit c3f63301")
 	public void shouldReturnDuplicatesPatients() throws Exception {
+		// given
+		Person person1 = new Person();
+		person1.addName(new PersonName("Ioan", "Theo", "Fletcher"));
+		person1.setGender("M");
+		person1.setBirthdate(Date.valueOf("2021-06-26"));
+		person1 = hibernatePersonDAO.savePerson(person1);
+		Patient patient1 = new Patient(person1);
+		patient1.addIdentifier(new PatientIdentifier("101X", null, null));
+		patient1.setVoided(true);
+		hibernatePatientDao.savePatient(patient1);
+
+		Person person2 = new Person();
+		person2.addName(new PersonName("Ioan", "Theo", "Fletcher"));
+		person2.setGender("M");
+		person2.setBirthdate(Date.valueOf("2021-06-26"));
+		person2 = hibernatePersonDAO.savePerson(person2);
+		Patient patient2 = new Patient(person2);
+		patient2.addIdentifier(new PatientIdentifier("101X", null, null));
+		patient2.setVoided(true);
+		hibernatePatientDao.savePatient(patient2);
+
+		Context.flushSession();
+
+		// when
 		MockHttpServletRequest req = request(RequestMethod.GET, "patient");
-		req.addParameter("attributesToFindDuplicatesBy", "middleName,gender");
+		req.addParameter("attributesToFindDuplicatesBy", "givenName,middleName,familyName,gender,identifier,birthdate,includeVoided");
 
 		SimpleObject result = deserialize(handle(req));
 
-		assertEquals(4, Util.getResultsSize(result));
+		// then
+		assertEquals(2, Util.getResultsSize(result));
 	}
 
 }
