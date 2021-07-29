f50abdede7dd8bf7a53918ee1b9e7624793612f4
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2021-07-29 22:50:32 +0200

    RESTWS-832: Update REST resource for Field Types management (#495)
    * RESTWS-832: Add REST resource for Field Types management
    
    * RESTWS-832: Add license

--- omod-1.8/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/FieldTypeController1_8Test.java
+++ omod-1.8/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/FieldTypeController1_8Test.java
@@ -10,8 +10,11 @@
 package org.openmrs.module.webservices.rest.web.v1_0.controller.openmrs1_8;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -29,9 +32,9 @@ import org.apache.commons.beanutils.PropertyUtils;
  * Tests functionality of {@link FieldTypeController}.
  */
 public class FieldTypeController1_8Test extends MainResourceControllerTest {
-	
+
 	private FormService service;
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceControllerTest#getURI()
 	 */
@@ -39,7 +42,7 @@ public class FieldTypeController1_8Test extends MainResourceControllerTest {
 	public String getURI() {
 		return "fieldtype";
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceControllerTest#getUuid()
 	 */
@@ -47,7 +50,7 @@ public class FieldTypeController1_8Test extends MainResourceControllerTest {
 	public String getUuid() {
 		return RestTestConstants1_8.FIELD_TYPE_UUID;
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceControllerTest#getAllCount()
 	 */
@@ -55,12 +58,12 @@ public class FieldTypeController1_8Test extends MainResourceControllerTest {
 	public long getAllCount() {
 		return 2;
 	}
-	
+
 	@Before
 	public void before() {
 		this.service = Context.getFormService();
 	}
-	
+
 	@Test
 	public void shouldUnRetireAFieldType() throws Exception {
 		FieldType fieldType = service.getFieldTypeByUuid(getUuid());
@@ -69,14 +72,64 @@ public class FieldTypeController1_8Test extends MainResourceControllerTest {
 		service.saveFieldType(fieldType);
 		fieldType = service.getFieldTypeByUuid(getUuid());
 		assertTrue(fieldType.isRetired());
-		
+
 		String json = "{\"deleted\": \"false\"}";
 		SimpleObject response = deserialize(handle(newPostRequest(getURI() + "/" + getUuid(), json)));
-		
+
 		fieldType = service.getFieldTypeByUuid(getUuid());
 		assertFalse(fieldType.isRetired());
 		assertEquals("false", PropertyUtils.getProperty(response, "retired").toString());
-		
+
 	}
-	
+
+	@Test
+	public void shouldCreateNewFieldType() throws Exception {
+		int countBefore = service.getAllFieldTypes().size();
+
+		String json = "{\"name\": \"test11\",\"description\": \"test\",\"isSet\": false}";
+		SimpleObject response = deserialize(handle(newPostRequest(getURI(), json)));
+
+		String uuid = response.get("uuid");
+		FieldType createdFieldType = service.getFieldTypeByUuid(uuid);
+
+		assertNotNull(createdFieldType);
+		assertEquals(countBefore + 1, service.getAllFieldTypes().size());
+		assertEquals("test11", createdFieldType.getName());
+		assertEquals("test", createdFieldType.getDescription());
+		assertEquals(false, createdFieldType.getIsSet());
+	}
+
+	@Test
+	public void shouldUpdateFieldType() throws Exception {
+		FieldType existingFieldType = new FieldType();
+		existingFieldType.setName("field type");
+		existingFieldType.setDescription("desc");
+		existingFieldType.setIsSet(false);
+		service.saveFieldType(existingFieldType);
+
+		String json = "{\"name\": \"field type\",\"description\": \"desc\",\"isSet\": true}";
+		handle(newPostRequest(getURI() + "/" + existingFieldType.getUuid(), json));
+		FieldType updatedFieldType = service.getFieldTypeByUuid(existingFieldType.getUuid());
+
+		assertNotNull(updatedFieldType);
+		assertEquals("field type", updatedFieldType.getName());
+		assertEquals("desc", updatedFieldType.getDescription());
+		assertEquals(true, updatedFieldType.getIsSet());
+	}
+
+	@Test
+	public void shouldPurgeFieldType() throws Exception {
+		FieldType existingFieldType = new FieldType();
+		existingFieldType.setName("field type");
+		existingFieldType.setDescription("desc");
+		existingFieldType.setIsSet(false);
+		service.saveFieldType(existingFieldType);
+
+		int countBefore = service.getAllFieldTypes().size();
+		handle(newDeleteRequest(getURI() + "/" + existingFieldType.getUuid(), new Parameter("purge", "true")));
+
+		FieldType deletedFieldType = service.getFieldTypeByUuid(existingFieldType.getUuid());
+		assertNull(deletedFieldType);
+		assertEquals(countBefore - 1, service.getAllFieldTypes().size());
+	}
 }
