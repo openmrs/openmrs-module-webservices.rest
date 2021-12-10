c126595d44d615727fbf83d4e78562fbbc269374
abertnamanya <abertnamanya@gmail.com>
2021-12-10 09:04:09 +0300

    Authentication Filter should not return 403

--- omod-common/src/main/java/org/openmrs/module/webservices/rest/web/filter/AuthorizationFilter.java
+++ omod-common/src/main/java/org/openmrs/module/webservices/rest/web/filter/AuthorizationFilter.java
@@ -3,7 +3,7 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
  * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
+ * <p>
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
@@ -37,71 +37,76 @@ import org.openmrs.module.webservices.rest.web.RestUtil;
  * {@link RestConstants#ALLOWED_IPS_GLOBAL_PROPERTY_NAME}
  */
 public class AuthorizationFilter implements Filter {
-	
-	protected final Log log = LogFactory.getLog(getClass());
-	
-	/**
-	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
-	 */
-	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		log.debug("Initializing REST WS Authorization filter");
-	}
-	
-	/**
-	 * @see javax.servlet.Filter#destroy()
-	 */
-	@Override
-	public void destroy() {
-		log.debug("Destroying REST WS Authorization filter");
-	}
-	
-	/**
-	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
-	 *      javax.servlet.ServletResponse, javax.servlet.FilterChain)
-	 */
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-	        ServletException {
-		
-		// check the IP address first.  If its not valid, return a 403
-		if (!RestUtil.isIpAllowed(request.getRemoteAddr())) {
-			// the ip address is not valid, set a 403 http error code
-			HttpServletResponse httpresponse = (HttpServletResponse) response;
-			httpresponse.sendError(HttpServletResponse.SC_FORBIDDEN, "IP address '" + request.getRemoteAddr()
-			        + "' is not authorized");
-			return;
-		}
-		
-		// skip if the session has timed out, we're already authenticated, or it's not an HTTP request
-		if (request instanceof HttpServletRequest) {
-			HttpServletRequest httpRequest = (HttpServletRequest) request;
-			if (httpRequest.getRequestedSessionId() != null && !httpRequest.isRequestedSessionIdValid()) {
-				HttpServletResponse httpResponse = (HttpServletResponse) response;
-				httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Session timed out");
-			}
-			
-			if (!Context.isAuthenticated()) {
-				String basicAuth = httpRequest.getHeader("Authorization");
-				if (basicAuth != null) {
-					// this is "Basic ${base64encode(username + ":" + password)}"
-					try {
-						basicAuth = basicAuth.substring(6); // remove the leading "Basic "
-						String decoded = new String(Base64.decodeBase64(basicAuth), Charset.forName("UTF-8"));
-						String[] userAndPass = decoded.split(":");
-						Context.authenticate(userAndPass[0], userAndPass[1]);
-						if (log.isDebugEnabled())
-							log.debug("authenticated " + userAndPass[0]);
-					}
-					catch (Exception ex) {
-						// This filter never stops execution. If the user failed to
-						// authenticate, that will be caught later.
-					}
-				}
-			}
-		}
-		
-		// continue with the filter chain (unless IP is not allowed)
-		chain.doFilter(request, response);
-	}
+
+    protected final Log log = LogFactory.getLog(getClass());
+
+    /**
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+        log.debug("Initializing REST WS Authorization filter");
+    }
+
+    /**
+     * @see javax.servlet.Filter#destroy()
+     */
+    @Override
+    public void destroy() {
+        log.debug("Destroying REST WS Authorization filter");
+    }
+
+    /**
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
+     *      javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+
+        // check the IP address first.  If its not valid, return a 403
+        if (!RestUtil.isIpAllowed(request.getRemoteAddr())) {
+            // the ip address is not valid, set a 403 http error code
+            HttpServletResponse httpresponse = (HttpServletResponse) response;
+            httpresponse.sendError(HttpServletResponse.SC_FORBIDDEN, "IP address '" + request.getRemoteAddr()
+                    + "' is not authorized");
+            return;
+        }
+
+        // skip if the session has timed out, we're already authenticated, or it's not an HTTP request
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            if (httpRequest.getRequestedSessionId() != null && !httpRequest.isRequestedSessionIdValid()) {
+                HttpServletResponse httpResponse = (HttpServletResponse) response;
+                httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Session timed out");
+            }
+
+            if (!Context.isAuthenticated()) {
+                String basicAuth = httpRequest.getHeader("Authorization");
+                if (basicAuth != null) {
+                    // this is "Basic ${base64encode(username + ":" + password)}"
+                    try {
+                        basicAuth = basicAuth.substring(6); // remove the leading "Basic "
+                        String decoded = new String(Base64.decodeBase64(basicAuth), Charset.forName("UTF-8"));
+                        String[] userAndPass = decoded.split(":");
+                        Context.authenticate(userAndPass[0], userAndPass[1]);
+                        if (log.isDebugEnabled())
+                            log.debug("authenticated " + userAndPass[0]);
+                    } catch (Exception ex) {
+                        // This filter never stops execution. If the user failed to
+                        // authenticate, that will be caught later.
+                    }
+                } else {
+                    HttpServletResponse httpResponse = (HttpServletResponse) response;
+                    httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "Basic header does not have a provided credential");
+                }
+            } else {
+                HttpServletResponse httpResponse = (HttpServletResponse) response;
+                httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Wrong credentials provided");
+            }
+        }
+
+        // continue with the filter chain (unless IP is not allowed)
+        chain.doFilter(request, response);
+    }
 }
