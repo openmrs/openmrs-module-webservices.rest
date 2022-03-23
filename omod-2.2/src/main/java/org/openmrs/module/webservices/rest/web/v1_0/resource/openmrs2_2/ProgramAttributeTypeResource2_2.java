ed550108a3e92e5d4b18fce41e3c6a4b9f0c7e6f
binduak <tbindu@thoughtworks.com>
2022-03-24 00:55:24 +0530

    Bindu | RESTWS-888 | Add support for program Attribute Type Concept

--- omod-2.2/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs2_2/ProgramAttributeTypeResource2_2.java
+++ omod-2.2/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs2_2/ProgramAttributeTypeResource2_2.java
@@ -9,10 +9,14 @@
  */
 package org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs2_2;
 
+import org.openmrs.Concept;
+import org.openmrs.ConceptDatatype;
 import org.openmrs.ProgramAttributeType;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.web.ConversionUtil;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.PropertyGetter;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.representation.RefRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
@@ -20,14 +24,17 @@ import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceD
 import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9.BaseAttributeTypeCrudResource1_9;
+import org.openmrs.util.OpenmrsUtil;
+
+import java.util.regex.Pattern;
 
 @Resource(name = RestConstants.VERSION_1 + "/programattributetype", supportedClass = ProgramAttributeType.class, supportedOpenmrsVersions = {
         "2.2.*", "2.3.*", "2.4.*", "2.5.*", "2.6.*" })
 public class ProgramAttributeTypeResource2_2 extends BaseAttributeTypeCrudResource1_9<ProgramAttributeType> {
-	
+
 	@Override
 	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
-		
+
 		if (rep instanceof RefRepresentation) {
 			DelegatingResourceDescription description = new DelegatingResourceDescription();
 			description.addProperty("uuid");
@@ -39,31 +46,47 @@ public class ProgramAttributeTypeResource2_2 extends BaseAttributeTypeCrudResour
 		}
 		return super.getRepresentationDescription(rep);
 	}
-	
+
 	@Override
 	public ProgramAttributeType getByUniqueId(String uuid) {
 		return Context.getProgramWorkflowService().getProgramAttributeTypeByUuid(uuid);
 	}
-	
+
 	@Override
 	public ProgramAttributeType newDelegate() {
 		return new ProgramAttributeType();
 	}
-	
+
 	@Override
 	public ProgramAttributeType save(ProgramAttributeType programAttributeType) {
 		return Context.getProgramWorkflowService().saveProgramAttributeType(programAttributeType);
 	}
-	
+
 	@Override
 	public void purge(ProgramAttributeType programAttributeType, RequestContext requestContext) throws ResponseException {
 		Context.getProgramWorkflowService().purgeProgramAttributeType(programAttributeType);
 	}
-	
+
 	@Override
 	protected NeedsPaging<ProgramAttributeType> doGetAll(RequestContext context) throws ResponseException {
 		return new NeedsPaging<ProgramAttributeType>(Context.getProgramWorkflowService()
 		        .getAllProgramAttributeTypes(),
 		        context);
+	}
+
+	@PropertyGetter("concept")
+	public Object getConcept(ProgramAttributeType delegate) {
+		if (!OpenmrsUtil.nullSafeEquals(delegate.getDatatypeClassname(), ConceptDatatype.class.getCanonicalName()))
+			return null;
+		else {
+			Concept concept;
+			String id = delegate.getDatatypeConfig();
+			if (Pattern.compile("\\d+").matcher(id).matches()) {
+				concept = Context.getConceptService().getConcept(Integer.valueOf(id));
+			} else {
+				concept = Context.getConceptService().getConceptByUuid(id);
+			}
+			return ConversionUtil.convertToRepresentation(concept, Representation.FULL);
+		}
 	}
 }
