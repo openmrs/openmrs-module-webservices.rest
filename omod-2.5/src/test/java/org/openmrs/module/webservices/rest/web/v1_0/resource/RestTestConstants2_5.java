51e99274b0668e7728114576eba8120f042e5d14
slubwama1 <lubwamasamuel3@gmail.com>
2023-09-10 20:34:45 +0300

    removing unnecessary tests and files

--- omod-2.5/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/resource/RestTestConstants2_5.java
+++ omod-2.5/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/resource/RestTestConstants2_5.java
@@ -13,12 +13,10 @@ public class RestTestConstants2_5 {
 	
 	public final static String TEST_DATASET = "customTestDataset2_5.xml";
 	
-	public final static String LOCATION_UUID = "f08ba64b-ea57-4a41-nfdr-9dfc59b0c60a";
+	public final static String ORDER_UUID = "204897de-70c6-4d5f-9051-b269fbd0700c";
 	
 	public final static String ORDER_ATTRIBUTE_UUID = "3a2bdb18-6faa-11e0-8414-001e378eb67e";
 	
 	public final static String ORDER_ATTRIBUTE_TYPE_UUID = "9516cc50-6f9f-11e0-8414-001e378eb67e";
-	
-	public final static String LOCATION_ATTRIBUTE_TYPE2_UUID = "9516cc50-6f9f-132r-5433-001e378eb67f";
 	
 }
