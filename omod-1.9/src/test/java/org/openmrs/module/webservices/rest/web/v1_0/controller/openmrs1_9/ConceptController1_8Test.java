8ffb7a48cf74025552d949ee89b16afc8de3263f
josephbate <sjb.lav@gmail.com>
2023-10-11 20:51:56 +0300

    testing filtering concept by Class concept uuid

--- omod-1.9/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_9/ConceptController1_8Test.java
+++ omod-1.9/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_9/ConceptController1_8Test.java
@@ -333,6 +333,17 @@ public class ConceptController1_8Test extends MainResourceControllerTest {
 		assertThat(hits, contains(hasUuid("15f83cd6-64e9-4e06-a5f9-364d3b14a43d")));
 	}
 
+	@Test
+	public void shouldSearchAndReturnConceptsByClass() throws Exception {
+		service.updateConceptIndex(service.getConceptByUuid("15f83cd6-64e9-4e06-a5f9-364d3b14a43d"));
+		SimpleObject response = deserialize(handle(newGetRequest(getURI(), new Parameter("class", "3d065ed4-b0b9-4710-9a17-6d8c4fd259b7"))));
+		List<Object> results = Util.getResultsList(response);
+		
+		Assert.assertEquals(2, results.size());
+		Object next = results.iterator().next();
+		Assert.assertThat((String) PropertyUtils.getProperty(next, "uuid"), is("15f83cd6-64e9-4e06-a5f9-364d3b14a43d"));
+		System.out.println(results);
+	}
 
 	@Test(expected = IllegalStateException.class)
 	public void shouldThrowExceptionWhenSearchRequiredParametersAreCalledTwice() throws Exception {
