2fb74513018d6fa6e83d36f5e4e94f1e16b42010
dkayiwa <dkayiwa@openmrs.org>
2025-04-23 02:05:07 +0300

    RESTWS-976 get reference range for a concept and patient

--- omod-2.7/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs2_7/RestConstants2_7.java
+++ omod-2.7/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs2_7/RestConstants2_7.java
@@ -14,4 +14,12 @@ public class RestConstants2_7 {
 	public static final String RESOURCE_VERSION = "2.7";
 	
 	public static final String OBS_REFERENCE_RANGE_UUID = "b4166639-24d5-4d13-941d-1c58a225da9b";
+	
+	public static final String CONCEPT_REFERENCE_RANGE_UUID = "239c1904-15ff-45e1-ac9d-d83afb637926";
+	
+	public static final String PATIENT_UUID = "5946f880-b197-400b-9caa-a3c661d23041";
+	
+	public static final String CONCEPT_NUMERIC_UUID = "a334a29c-0267-474e-8426-df0040b1da57";
+	
+	public static final String DISPLAY_DIASTOLIC_BLOOD_PRESSURE = "Diastolic blood pressure";
 }
---
+++ omod-2.7/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs2_7/ConceptReferenceRangeController2_7Test.java
@@ -0,0 +1,72 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.webservices.rest.web.v1_0.controller.openmrs2_7;
+
+import java.util.List;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.test.Util;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.jupiter.MainResourceControllerTest;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs2_7.RestConstants2_7;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+public class ConceptReferenceRangeController2_7Test extends MainResourceControllerTest {
+
+	@Override
+	public String getURI() {
+		return "conceptreferencerange";
+	}
+
+	@Override
+	public String getUuid() {
+		return RestConstants2_7.CONCEPT_REFERENCE_RANGE_UUID;
+	}
+
+	@Override
+	public long getAllCount() {
+		return 0;
+	}
+
+	@Test
+	public void shouldGetConceptReferenceRange() throws Exception {
+		MockHttpServletRequest request = newGetRequest(getURI());
+		request.setParameter("patient", RestConstants2_7.PATIENT_UUID);
+		request.setParameter("concept", RestConstants2_7.CONCEPT_NUMERIC_UUID);
+		request.setParameter("v", "full");
+		SimpleObject response = deserialize(handle(request));
+		List<Object> resultsList = Util.getResultsList(response);
+		Assert.assertEquals(1, resultsList.size());
+		Assert.assertEquals(140.0, PropertyUtils.getProperty(resultsList.get(0), "hiAbsolute"));
+		Assert.assertEquals(70.0, PropertyUtils.getProperty(resultsList.get(0), "lowAbsolute"));
+	}
+
+	@Override
+	public void shouldGetDefaultByUuid() throws Exception {
+		super.shouldGetDefaultByUuid();
+	}
+
+	@Override
+	public void shouldGetRefByUuid() throws Exception {
+		super.shouldGetRefByUuid();
+	}
+
+	@Override
+	public void shouldGetFullByUuid() throws Exception {
+		super.shouldGetFullByUuid();
+	}
+
+	@Override
+	public void shouldGetAll() throws Exception {
+		super.shouldGetAll();
+	}
+}
