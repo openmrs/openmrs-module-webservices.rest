c9ffe2cdccaf24b75a95b0add00bb68dbfd62bbe
capernix <marvinsukumar1@gmail.com>
2025-05-19 21:39:34 +0530

    Added a working Mojo Plugin

---
+++ tools/openapi-generator-maven-plugin/src/main/java/org/openmrs/plugin/HelloMojo.java
@@ -0,0 +1,25 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+
+package org.openmrs.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "hello", defaultPhase = LifecyclePhase.PROCESS_CLASSES)
+public class HelloMojo extends AbstractMojo {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Hello from OpenMRS Maven Plugin!");
+    }
+}
