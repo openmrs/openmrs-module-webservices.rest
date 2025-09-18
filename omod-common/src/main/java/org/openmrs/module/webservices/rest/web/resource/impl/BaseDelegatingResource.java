fbb7af6ceb5b2ebed84b39e9b82be82b60d09940
mogoodrich <mgoodrich@pih.org>
2025-09-18 16:41:38 -0400

    RESTWS-990: Allow audit data to be creatable (Abstract out and use for delegating sub resource)

--- omod-common/src/main/java/org/openmrs/module/webservices/rest/web/resource/impl/BaseDelegatingResource.java
+++ omod-common/src/main/java/org/openmrs/module/webservices/rest/web/resource/impl/BaseDelegatingResource.java
@@ -29,7 +29,10 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.proxy.HibernateProxy;
+import org.openmrs.Changeable;
 import org.openmrs.OpenmrsObject;
+import org.openmrs.Retireable;
+import org.openmrs.Voidable;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.ModuleUtil;
 import org.openmrs.module.webservices.rest.SimpleObject;
@@ -847,5 +850,39 @@ public abstract class BaseDelegatingResource<T> extends BaseDelegatingConverter<
 	public T newDelegate(SimpleObject object) {
 		return newDelegate();
 	}
-	
+
+
+	protected DelegatingResourceDescription buildCreatableProperties(T delegate, DelegatingResourceHandler<? extends T> handler,  SimpleObject propertiesToCreate) {
+
+		DelegatingResourceDescription description = handler.getCreatableProperties();
+		if (propertiesToCreate.containsKey(RestConstants.PROPERTY_UUID)) {
+			description.addProperty(RestConstants.PROPERTY_UUID);
+		}
+
+		if (Context.hasPrivilege(RestConstants.PRIV_SET_AUDIT_DATA)) {
+			if (delegate instanceof org.openmrs.Creatable) {
+				description.addProperty("dateCreated");
+				description.addProperty("creator");
+			}
+			if (delegate instanceof Changeable) {
+				description.addProperty("dateChanged");
+				description.addProperty("changedBy");
+			}
+			if (delegate instanceof Retireable) {
+				description.addProperty("retired");
+				description.addProperty("retireReason");
+				description.addProperty("retiredBy");
+				description.addProperty("dateRetired");
+
+			}
+			if (delegate instanceof Voidable) {
+				description.addProperty("voided");
+				description.addProperty("voidReason");
+				description.addProperty("voidedBy");
+				description.addProperty("dateVoided");
+			}
+		}
+
+		return description;
+	}
 }
