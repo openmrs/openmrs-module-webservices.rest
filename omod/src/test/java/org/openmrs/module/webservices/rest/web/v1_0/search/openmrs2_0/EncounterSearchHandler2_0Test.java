6e62c7a2f091a0b798124413773315cc80b54bcd
chibongho <cbho@pih.org>
2025-10-10 06:03:37 -0400

    RESTWS-996 Add route to search resources by searchHandlerId (#678)
    * RESTWS-996 Add route to search resources by searchHandlerId
    
    * add tests
    
    * comments

--- omod/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs2_0/EncounterSearchHandler2_0Test.java
+++ omod/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/search/openmrs2_0/EncounterSearchHandler2_0Test.java
@@ -29,7 +29,7 @@ public class EncounterSearchHandler2_0Test extends RestControllerTestUtils {
 
     /**
      * @verifies returns encounters and totalCount filtered by patient uuid only
-     * @see @see EncounterSearchHandler2_0#search(RequestContext)
+     * @see EncounterSearchHandler2_0#search(RequestContext)
      * @throws Exception
      */
     @Test
@@ -49,7 +49,7 @@ public class EncounterSearchHandler2_0Test extends RestControllerTestUtils {
 
     /**
      * @verifies returns encounters and totalCount filtered by patient uuid and encounterType uuid
-     * @see @see EncounterSearchHandler2_0#search(RequestContext)
+     * @see EncounterSearchHandler2_0#search(RequestContext)
      * @throws Exception
      */
     @Test
@@ -72,12 +72,37 @@ public class EncounterSearchHandler2_0Test extends RestControllerTestUtils {
      * @verifies returns encounters and totalCount filtered by patient uuid and limit
      * i.e. (limit 1, results size should not be the same as totalCount)
      *
-     * @see @see EncounterSearchHandler2_0#search(RequestContext)
+     * @see EncounterSearchHandler2_0#search(RequestContext)
      * @throws Exception
      */
     @Test
     public void search_shouldReturnEncountersWithTotalCountFilteredByPatientLimitedToOne() throws Exception {
         MockHttpServletRequest req = request(RequestMethod.GET, getURI());
+        req.addParameter("patient", RestTestConstants1_9.PATIENT_WITH_OBS_UUID);
+        req.addParameter("limit", String.valueOf(1));
+        req.addParameter("totalCount", String.valueOf(Boolean.TRUE));
+
+        SimpleObject result = deserialize(handle(req));
+        List<Encounter> encounters = result.get("results");
+        Assert.assertEquals(1, encounters.size());
+        int totalCount = result.get("totalCount");
+        Assert.assertNotNull(totalCount);
+        Assert.assertEquals(3, totalCount);
+        Assert.assertNotEquals(encounters.size(), totalCount);
+    }
+
+    /**
+     * @verifies returns encounters and totalCount filtered by patient uuid and limit
+     * i.e. (limit 1, results size should not be the same as totalCount). Same as
+     * previous test, but invokes the default SearchHandler explicitly via the URI. 
+     *
+     * @see EncounterSearchHandler2_0#search(RequestContext)
+     * @throws Exception
+     */
+    @Test
+    public void defaultSearchHandler_shouldReturnEncountersWithTotalCountFilteredByPatientLimitedToOne() throws Exception {
+        String searchHandlerURI = getURI() + "/search/default";
+        MockHttpServletRequest req = request(RequestMethod.GET, searchHandlerURI);
         req.addParameter("patient", RestTestConstants1_9.PATIENT_WITH_OBS_UUID);
         req.addParameter("limit", String.valueOf(1));
         req.addParameter("totalCount", String.valueOf(Boolean.TRUE));
