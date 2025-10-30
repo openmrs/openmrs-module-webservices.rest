1feadd7b8406d7ba739f2bd6bd7969af4afd35d4
mogoodrich <mgoodrich@pih.org>
2025-10-30 15:44:38 -0400

    Revert "RESTWS-999: Add REST support for ProviderRole added in OpenMRS 2.8"
    This reverts commit fd04184c21fc73e4ab3b78c50d3c786cde4f8645.

--- omod/src/test/java/org/openmrs/module/webservices/rest/web/api/impl/RestServiceImplTest.java
+++ omod/src/test/java/org/openmrs/module/webservices/rest/web/api/impl/RestServiceImplTest.java
@@ -9,7 +9,6 @@
  */
 package org.openmrs.module.webservices.rest.web.api.impl;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -31,7 +30,6 @@ import org.mockingbird.test.rest.resource.InstantiateExceptionAnimalResource_1_9
 import org.mockingbird.test.rest.resource.UnannotatedAnimalResource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.openmrs.api.APIException;
 import org.openmrs.module.webservices.rest.web.OpenmrsClassScanner;
@@ -84,21 +82,13 @@ public class RestServiceImplTest {
 	
 	@InjectMocks
 	RestService restService = new RestServiceImpl();
-
+	
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
 
-	private AutoCloseable closeableMocks;
-
 	@Before
-	public void setUp() {
-		closeableMocks  = MockitoAnnotations.openMocks(this);
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		closeableMocks.close();
-		Mockito.framework().clearInlineMocks();
+	public void initMocks() {
+		MockitoAnnotations.initMocks(this);
 	}
 	
 	/**
