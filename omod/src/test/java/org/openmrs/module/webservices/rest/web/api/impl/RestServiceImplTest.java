07fec4c61e6a8114e2bedf7afa7e4ae598cb3edb
mogoodrich <mgoodrich@pih.org>
2025-10-30 15:44:46 -0400

    Revert "RESTWS-999: Add REST support for ProviderRole added in OpenMRS 2.8"
    This reverts commit 8096a025370880506031a84a496490691656aaa1.

--- omod/src/test/java/org/openmrs/module/webservices/rest/web/api/impl/RestServiceImplTest.java
+++ omod/src/test/java/org/openmrs/module/webservices/rest/web/api/impl/RestServiceImplTest.java
@@ -9,7 +9,6 @@
  */
 package org.openmrs.module.webservices.rest.web.api.impl;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -30,8 +29,8 @@ import org.mockingbird.test.rest.resource.InstantiateExceptionAnimalResource_1_9
 import org.mockingbird.test.rest.resource.UnannotatedAnimalResource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.openmrs.api.APIException;
+import org.openmrs.module.webservices.RestBaseContextMockTest;
 import org.openmrs.module.webservices.rest.web.OpenmrsClassScanner;
 import org.openmrs.module.webservices.rest.web.RestUtil;
 import org.openmrs.module.webservices.rest.web.api.RestHelperService;
@@ -72,7 +71,7 @@ import static org.mockito.Mockito.when;
 /**
  * Tests {@link RestServiceImpl}.
  */
-public class RestServiceImplTest {
+public class RestServiceImplTest extends RestBaseContextMockTest {
 	
 	@Mock
 	RestHelperService restHelperService;
@@ -85,11 +84,6 @@ public class RestServiceImplTest {
 	
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
-
-	@Before
-	public void initMocks() {
-		MockitoAnnotations.initMocks(this);
-	}
 	
 	/**
 	 * @verifies return default representation if given null
