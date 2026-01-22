e557673a15621f750ef635497b350b17f6afc82b
Ujjawal Prabhat <UjjawalPrabhat1@gmail.com>
2026-01-22 16:15:35 +0530

    Format code and ensure proper line endings

--- omod-2.7/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs2_7/ConceptReferenceRangeResource2_7Test.java
+++ omod-2.7/src/test/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs2_7/ConceptReferenceRangeResource2_7Test.java
@@ -17,7 +17,7 @@ import org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResou
  * Tests functionality of {@link ConceptReferenceRangeResource2_7}.
  */
 public class ConceptReferenceRangeResource2_7Test extends BaseDelegatingResourceTest<ConceptReferenceRangeResource2_7, ConceptReferenceRange> {
-
+	
 	@Override
 	public ConceptReferenceRange newObject() {
 		ConceptReferenceRange referenceRange = new ConceptReferenceRange();
@@ -25,7 +25,7 @@ public class ConceptReferenceRangeResource2_7Test extends BaseDelegatingResource
 		referenceRange.setConceptNumeric(Context.getConceptService().getConceptNumericByUuid(RestConstants2_7.CONCEPT_NUMERIC_UUID));
 		return referenceRange;
 	}
-
+	
 	@Override
 	public void validateDefaultRepresentation() throws Exception {
 		super.validateDefaultRepresentation();
@@ -42,7 +42,7 @@ public class ConceptReferenceRangeResource2_7Test extends BaseDelegatingResource
 		assertPropEquals("allowDecimal", getObject().getConceptNumeric().getAllowDecimal());
 		assertPropEquals("isCriteriaBased", true);
 	}
-
+	
 	@Override
 	public void validateFullRepresentation() throws Exception {
 		super.validateFullRepresentation();
